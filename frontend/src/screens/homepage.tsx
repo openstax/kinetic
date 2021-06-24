99db3c53d66ea76f73142b5f98fcc17927596bfc
Nathan Stitt <nathan@stitt.org>
2021-06-24 14:13:50 -0500

    add front-end to docker-compose

--- frontend/src/screens/homepage.tsx
+++ frontend/src/screens/homepage.tsx
@@ -1,11 +1,13 @@
 import { React } from '../common'
 import { PageHeader } from '../PageHeader'
+import { ENV } from '../lib/env'
 
 export const Home = () => {
     return (
         <div className="">
             <PageHeader />
             <h3>hello world</h3>
+            <p>Api is: {ENV.API_URL}</p>
         </div>
     )
     
