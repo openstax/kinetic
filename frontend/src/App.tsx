3e83d543b1607dc9b44031c6f9098e0985a4a510
Nathan Stitt <nathan@stitt.org>
2021-06-24 14:26:08 -0500

    Rename "rubocop" action to "lint", add FE
    Adds FE eslint and typecheck

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,17 @@
-import { React, useState } from './common'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { React } from './common'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home } from './screens/homepage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <Router>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Router>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Router>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Router>
+        </div>
+    )
 }
 
 export default App
