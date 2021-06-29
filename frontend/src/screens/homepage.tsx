42ce5177fdf97bbc6583bb80582fe8dd00682dc9
Nathan Stitt <nathan@stitt.org>
2021-06-28 20:44:08 -0500

    link to dev console when local

--- frontend/src/screens/homepage.tsx
+++ frontend/src/screens/homepage.tsx
@@ -1,13 +1,15 @@
 import { React } from '../common'
 import { PageHeader } from '../PageHeader'
 import { ENV } from '../lib/env'
+import { Link } from 'react-router-dom'
 
 export default function Homepage() {
     return (
-        <div className="">
+        <div className="container homepage mt-4">
             <PageHeader />
             <h3>hello world</h3>
             <p>Api is: {ENV.API_URL}</p>
+            {ENV.IS_LOCAL && <Link className="btn btn-primary" to="/dev">Development Console</Link>}
         </div>
     )
 
