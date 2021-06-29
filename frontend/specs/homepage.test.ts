870717af7f848230e5b7d693646944d1ba5bb7ba
Nathan Stitt <nathan@stitt.org>
2021-06-28 21:30:27 -0500

    fix css class for homepage

--- frontend/specs/homepage.test.ts
+++ frontend/specs/homepage.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
     await page.goto('http://localhost:4000/')
-    expect(await page.textContent('.App')).toContain('hello world');
+    expect(await page.textContent('.homepage')).toContain('hello world');
 });
