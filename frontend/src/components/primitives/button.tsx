f48f8504c9f8f3d6ea656948f003c2f0bb556e64
Nathan Stitt <nathan@stitt.org>
2021-06-28 22:17:04 -0500

    add link to hom from dev console

--- frontend/src/components/primitives/button.tsx
+++ frontend/src/components/primitives/button.tsx
@@ -1,5 +1,5 @@
 import { React, cx } from '../../common'
-
+import { useHistory } from 'react-router-dom'
 import { BSVariants, bsClassNames } from './bs'
 
 interface ButtonProps extends BSVariants, React.HTMLProps<HTMLButtonElement> {
@@ -7,5 +7,19 @@ interface ButtonProps extends BSVariants, React.HTMLProps<HTMLButtonElement> {
 }
 export const Button:React.FC<ButtonProps> = ({ className, ...otherProps }) => {
     const [bsClasses, props] = bsClassNames('btn', otherProps, { default: 'light' })
-    return <button className={cx(className, bsClasses)} {...props} />
+    return (
+        <button
+            css={{ display: 'inline-flex', alignItems: 'center' }}
+            className={cx('btn', className, bsClasses)} {...props
+            } />
+    )
+}
+
+interface LinkButtonProps extends ButtonProps {
+    to: string
+}
+
+export const LinkButton:React.FC<LinkButtonProps> = ({ to, ...otherProps }) => {
+    const history = useHistory()
+    return <Button onClick={() => history.push(to)} {...otherProps} />
 }
--- frontend/src/screens/dev/index.tsx
+++ frontend/src/screens/dev/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { React } from '../../common'
 import { User, AvailableUsers } from './users'
+import { ChevronDoubleLeft } from '@emotion-icons/bootstrap'
+import { LinkButton } from '../../components/primitives'
 
 export default function Dev() {
     const [users, setUsers] = useState<AvailableUsers>(new AvailableUsers())
@@ -22,25 +24,36 @@ export default function Dev() {
     }
 
     return (
-        <div className="dev container mt-4">
-            {currentUser?.isValid && <h3>Logged in as: {currentUser.id}</h3>}
-            <div className="row">
-                <div className="col-8">
-                    <div className="card">
-                        <h5 className="card-header">Admins</h5>
-                        <ul className="list-group list-group-flush">
-                            {users.admins.map(u => (
-                                <a
-                                    key={u.id}
-                                    href='#'
-                                    data-user-id={u.id}
-                                    onClick={becomeUser}
-                                    className="list-group-item"
-                                >
-                                    {u.id}
-                                </a>
-                            ))}
-                        </ul>
+        <div className="dev-console">
+            <nav className="navbar fixed-top navbar-light py-1 bg-light">
+                <div className="container-fluid">
+                    <LinkButton secondary to="/">
+                        <ChevronDoubleLeft height="16" />
+                        Home
+                    </LinkButton>
+                </div>
+            </nav>
+            <div className="container mt-8">
+
+                {currentUser?.isValid && <h3>Logged in as: {currentUser.id}</h3>}
+                <div className="row">
+                    <div className="col-8">
+                        <div className="card">
+                            <h5 className="card-header">Admins</h5>
+                            <ul className="list-group list-group-flush">
+                                {users.admins.map(u => (
+                                    <a
+                                        key={u.id}
+                                        href='#'
+                                        data-user-id={u.id}
+                                        onClick={becomeUser}
+                                        className="list-group-item"
+                                    >
+                                        {u.id}
+                                    </a>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
