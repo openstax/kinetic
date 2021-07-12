aca1a2fc5ccc2d0a68badaeeabdb9a86149d73bd
Nathan Stitt <nathan@stitt.org>
2021-07-12 15:37:19 -0500

    remove jq and use provided API_ADDRESS

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -2,9 +2,10 @@ export const ENV = {
     MODE: import.meta.env.MODE || 'development',
     IS_DEV_MODE: import.meta.env.MODE == 'development',
     IS_PROD_MODE: import.meta.env.MODE == 'production',
-    API_URL: import.meta.env.VITE_API_URL || '',
+    API_ADDRESS: import.meta.env.VITE_API_ADDRESS || '',
     IS_LOCAL: window.location.hostname == 'localhost',
     ACCOUNTS_URL: 'https://accounts-dev.openstax.org/',
+    API_PATH: '/api/v0',
 }
 
 Object.freeze(ENV)
