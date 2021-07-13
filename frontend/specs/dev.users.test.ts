5f6b26df341cb92d5ef1da84e60aca11e69500bc
Nathan Stitt <nathan@stitt.org>
2021-07-13 11:16:51 -0500

    remove dev users spec, tested in studies already

--- frontend/specs/dev.users.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from './test'
-
-test('it can login as admin', async ({ page }) => {
-    await page.goto('http://localhost:4000/')
-    await page.click('testId=login-link')
-    await page.click('[data-user-id="00000000-0000-0000-0000-000000000000"]')
-    expect(await page.textContent('.homepage')).toContain('studies')
-});
