03d32ea6541f4a33e65eb4b8b8f1a5359f7f2ed5
Nathan Stitt <nathan@stitt.org>
2021-07-15 21:57:53 -0500

    use relative path when not in dev mode

--- frontend/src/lib/api-config.ts
+++ frontend/src/lib/api-config.ts
@@ -2,13 +2,11 @@ import { useMemo } from 'react'
 import { ENV } from './env'
 import { Configuration, StudiesApi } from '../api'
 
-console.log(ENV.API_ADDRESS)
-
 export const useStudyApi = () => {
 
     const api = useMemo(() => {
         const configuration = new Configuration({
-            basePath: ENV.API_ADDRESS + ENV.API_PATH,
+            basePath: ENV.API_PATH,
             credentials: 'include',
         })
         return new StudiesApi( configuration )
--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -1,11 +1,17 @@
+const IS_DEV_MODE = import.meta.env.MODE == 'development'
+const IS_LOCAL = window.location.hostname == 'localhost'
+const API_ADDRESS = IS_LOCAL ? 'http://localhost:4006' : ''
+const API_VERSION = 0
+
 export const ENV = {
+    IS_DEV_MODE,
+    IS_LOCAL,
+    API_VERSION,
     MODE: import.meta.env.MODE || 'development',
-    IS_DEV_MODE: import.meta.env.MODE == 'development',
     IS_PROD_MODE: import.meta.env.MODE == 'production',
-    API_ADDRESS: import.meta.env.VITE_API_ADDRESS || '',
-    IS_LOCAL: window.location.hostname == 'localhost',
+    API_ADDRESS,
+    API_PATH: `${API_ADDRESS}/api/v${API_VERSION}`,
     ACCOUNTS_URL: 'https://accounts-dev.openstax.org/',
-    API_PATH: '/api/v0',
 }
 
 Object.freeze(ENV)
