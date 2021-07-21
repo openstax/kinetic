8360babbe68112d4deb9951fd2fe4ce6818a4b46
Nathan Stitt <nathan@stitt.org>
2021-07-21 12:54:06 -0500

    redirect to accounts url specified by ENV

---
+++ frontend/src/components/incorrect-user.tsx
@@ -0,0 +1,25 @@
+import { React, useState, useEffect } from '@common'
+import { OXColoredStripe, LoadingAnimation } from '@components'
+import { Environment } from '../models/environment'
+
+export interface IncorrectUserProps {
+    desiredRole?: string
+}
+export const IncorrectUser:React.FC<IncorrectUserProps> = ({ desiredRole }) => {
+    const [env, setEnv] = useState<Environment>()
+    useEffect(() => {
+        Environment.fetch().then(e => setEnv(e))
+    }, [])
+
+    if (!env) return <LoadingAnimation />
+
+    return (
+        <div className="incorrect-user">
+            <OXColoredStripe />
+            <div className="container mt-4">
+                <h1>Looks like you‘re not  logged in{desiredRole ? ` as a ${desiredRole}` : ''}.</h1>
+                <p>Please <a data-test-id="login-link" href={env.accounts_url}>login</a> before using this site</p>
+            </div>
+        </div>
+    )
+}
