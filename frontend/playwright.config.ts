6f3a1c63271cb640068c288b9c361fcaf77c860c
Nathan Stitt <nathan@stitt.org>
2021-08-10 13:57:16 -0500

    tweaks to config for CI

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,10 +2,10 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     globalSetup: require.resolve('./specs/setup.ts'),
-
+    forbidOnly: !!process.env.CI,
     use: {
         screenshot: 'only-on-failure',
-
+        trace: 'retain-on-failure',
     },
 }
 
