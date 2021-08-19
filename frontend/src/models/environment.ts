c38b0a17abe0ed81f50fbe33b8b8fe51a3e64d24
Nathan Stitt <nathan@stitt.org>
2021-08-19 11:26:00 -0500

    add return to param

--- frontend/src/models/environment.ts
+++ frontend/src/models/environment.ts
@@ -19,14 +19,18 @@ export class Environment {
         this.accounts_env_name = payload.accounts_env_name || 'dev'
     }
 
+    get loginURL() {
+        const url = this.accounts_url
+        if (ENV.IS_DEV_MODE) return url
+
+        return `${url}/accounts/i/login/?r=${encodeURIComponent(window.location.href)}`
+    }
+
     get accounts_url() {
         if (ENV.IS_DEV_MODE) return '/dev/user'
         if (this.accounts_env_name == 'production') {
             return 'https://accounts.openstax.org'
         }
-        // TODO: add return to param once:
-        // 1) we figure out what it is https://openstax.slack.com/archives/C6NRD0CCQ/p1626814810461300
-        // 2) accounts is configured to allow labs as a valid return
         return `https://accounts-${this.accounts_env_name}.openstax.org`
     }
 
