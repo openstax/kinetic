6400dea434e8d451f703f3666bce5525b7460ec7
Nathan Stitt <nathan@stitt.org>
2021-08-27 12:46:40 -0500

    upgrade sentry gem

---
+++ backend/config/initializers/sentry.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+if Rails.env.production? || ENV['FORCE_ENABLE_SENTRY'] == 'true'
+  secrets = Rails.application.secrets.sentry
+  if secrets.nil?
+    warn 'No sentry secrets provided; not configuring Sentry!'
+    return
+  end
+  Sentry.init do |config|
+    config.dsn = secrets[:dsn]
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+    config.enabled_environments = %w[production]
+    config.environment = ENV['ENV_NAME'] || config.current_environment
+
+    # for performance, to see only 50% of all errors
+    # config.traces_sample_rate = 0.5
+
+  end
+
+end
