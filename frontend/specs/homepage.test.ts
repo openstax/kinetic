727b6cbfa1b86a3e73c096934a8edef4cf0f747c
Greg Fitch <greg.fitch@rice.edu>
2021-09-30 15:44:01 -0500

    update log in spec

--- frontend/specs/homepage.test.ts
+++ frontend/specs/homepage.test.ts
@@ -3,6 +3,6 @@ import { test, expect } from './test'
 test('requires login', async ({ page }) => {
     await page.goto('http://localhost:4000/')
     await page.waitForSelector('testId=incorrect-user-panel')
-    expect(await page.textContent('testId=incorrect-user-panel')).toContain('Please login');
+    expect(await page.textContent('testId=incorrect-user-panel')).toContain('Please log in');
     expect(await page.$('testId=login-link')).not.toBeNull()
 });
