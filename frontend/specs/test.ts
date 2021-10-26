e7b7067dfa7c409e1ffc771c3313e68c7f31d749
Nathan Stitt <nathan@stitt.org>
2021-10-26 17:07:28 -0500

    bump timeouts on CI

--- frontend/specs/test.ts
+++ frontend/specs/test.ts
@@ -3,17 +3,17 @@ import { expect } from '@playwright/test'
 import { matchers } from 'expect-playwright'
 import { TC, TestConfig } from './helpers'
 
-export const DEFAULT_TIMEOUT = 10000;
-export const DEFAULT_NAVIGATION_TIMEOUT = 15000;
+export const DEFAULT_TIMEOUT = process.env.CI ? 30000 : 10000
+export const DEFAULT_NAVIGATION_TIMEOUT = process.env.CI ? 30000 : 15000
 
 expect.extend(matchers)
 
 import * as faker from 'faker'
 export const createTestIdEngine = () => {
     const toTestSelector = (sel: string) => {
-        const quoted = sel.match(/^".*"$/) ? sel : `"${sel}"`;
-        return `[data-test-id=${quoted}]`;
-    };
+        const quoted = sel.match(/^".*"$/) ? sel : `"${sel}"`
+        return `[data-test-id=${quoted}]`
+    }
     return {
         query: (root: HTMLElement, selector: string) => root.querySelector(toTestSelector(selector)),
         queryAll: (root: HTMLElement, selector: string) => Array.from(root.querySelectorAll(toTestSelector(selector))),
@@ -28,11 +28,11 @@ test.beforeAll( async () => {
 })
 
 test.beforeEach(({ context, page }) => {
-    context.setDefaultTimeout(DEFAULT_TIMEOUT);
-    page.setDefaultTimeout(DEFAULT_TIMEOUT);
-    context.setDefaultNavigationTimeout(DEFAULT_NAVIGATION_TIMEOUT);
-    page.setDefaultNavigationTimeout(DEFAULT_NAVIGATION_TIMEOUT);
-});
+    context.setDefaultTimeout(DEFAULT_TIMEOUT)
+    page.setDefaultTimeout(DEFAULT_TIMEOUT)
+    context.setDefaultNavigationTimeout(DEFAULT_NAVIGATION_TIMEOUT)
+    page.setDefaultNavigationTimeout(DEFAULT_NAVIGATION_TIMEOUT)
+})
 
 export * from '@playwright/test'
 
