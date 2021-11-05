6488bc5f5c4d803ab5cbc5adc358aec62c4e4265
Nathan Stitt <nathan@stitt.org>
2021-11-05 15:44:37 -0500

    assign host variable

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -113,9 +113,11 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  host = Rails.application.secrets.host
+
   routes.default_url_options.merge!(
     {
-      host: Rails.application.secrets.host,
+      host: host,
       protocol: 'https'
     }
   )
