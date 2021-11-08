7cbccc7414b825667c0b61eae5d6346f74d09613
Nathan Stitt <nathan@stitt.org>
2021-11-08 11:00:41 -0600

    update sentry; rm enabled_enviroments
    enabled_environments needs to be symbole, but is also not needed since
    it's inside "if production?" block

--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
-#if Rails.env.production? || ENV['FORCE_ENABLE_SENTRY'] == 'true'
+if Rails.env.production? || ENV['FORCE_ENABLE_SENTRY'] == 'true'
   secrets = Rails.application.secrets.sentry
   if secrets.nil?
     warn 'No sentry secrets provided; not configuring Sentry!'
     return
   end
-p secrets
   Sentry.init do |config|
     config.dsn = secrets[:dsn]
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-    config.enabled_environments = %w[production]
     config.environment = ENV['ENV_NAME'] || Rails.application.secrets.accounts[:env_name]
 
-    # for performance, to see only 50% of all errors
-    # config.traces_sample_rate = 0.5
+    # for performance, to see only 50% of all errors use 0.5
+    config.traces_sample_rate = 1.0
 
+    config.traces_sampler = lambda do |context|
+      true
+    end
   end
-
-#end
+end
