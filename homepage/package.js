b458866c4abd532b49328a1d03875329772fd3b6
Nathan Stitt <nathan@stitt.org>
2021-11-19 09:27:04 -0600

    add homepage to repo so it can be tracked
    It's plain html that gets copy-n-pasted into wagtail, but is
    easier to develop locally then use the `copy-to-clipboard`
    script to grab the contents

---
+++ homepage/package.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const cheerio = require('cheerio');
+
+let data;
+
+try {
+  data = fs.readFileSync('./index.html', 'utf8')
+} catch (err) {
+  console.error(err)
+}
+
+const $ = cheerio.load(data);
+
+
+
+console.log(
+  $('body').html()
+)
