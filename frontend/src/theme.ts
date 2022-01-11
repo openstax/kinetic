445016fa338d132f8f4ac2e55a1e8c44e9549025
Nathan Stitt <nathan@stitt.org>
2022-01-11 13:17:31 -0800

    purple/lightGray

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -4,7 +4,8 @@ export const colors = {
     darkBlue: '#233066',
     red: '#ca2026',
     teal: '#0dc0de',
-
+    purple: '#6922EA',
+    lightGray: '#DBDBDB',
     text: '#212529',
     input: { border: '#ced4da' },
 
