d622813350ebf651c515c123fef353f6fe18ded8
Nathan Stitt <nathan@stitt.org>
2022-01-11 13:17:03 -0800

    add tooltip styles

--- frontend/src/components/popover.tsx
+++ frontend/src/components/popover.tsx
@@ -1,19 +1,27 @@
 import { React, useState, cx } from '../common'
 import { usePopper } from 'react-popper'
 import { useRefElement } from 'rooks'
+import * as CSS from 'csstype'
 
 interface ControlledPopoverProps {
     show: boolean
+    displayType?: 'tooltip' | 'popover'
     target?: HTMLElement | null
     title?: React.ReactNode
 }
 
 interface PopoverProps extends Omit<ControlledPopoverProps, 'show'> {
     popover: React.ReactNode
+    className?: string
+    style?: CSS.Properties<string | number>
 }
 
 export const ControlledPopover: React.FC<ControlledPopoverProps> = ({
-    show, title, target, children,
+    show,
+    title,
+    target,
+    children,
+    displayType = 'popover',
 }) => {
     const [setPopover, popover] = useRefElement<HTMLElement>()
     const [setArrow, arrow] = useRefElement<HTMLDivElement>()
@@ -25,28 +33,36 @@ export const ControlledPopover: React.FC<ControlledPopoverProps> = ({
     const pos = (attributes.popper || {})['data-popper-placement']
     return (
         <div
-            className={cx('popover', 'fade', `bs-popover-${pos}`, { show })}
+            className={cx(displayType, 'fade', `bs-${displayType}-${pos}`, { show })}
             role="tooltip"
             ref={setPopover}
             style={styles.popper}
             {...attributes.popper}
         >
-            <div className="popover-arrow" ref={setArrow} style={styles.arrow} />
-            {title && <h3 className="popover-header">{title}</h3>}
-            <div className="popover-body">
+            <div className={`${displayType}-arrow`} ref={setArrow} style={styles.arrow} />
+            {title && <h3 className={`${displayType}-header`}>{title}</h3>}
+            <div className={displayType == 'popover' ? 'popover-body' : 'tooltip-inner'}>
                 {children}
             </div>
         </div>
     )
 }
 
-export const Popover: React.FC<PopoverProps> = ({ popover, children, ...popoverProps }) => {
+export const Popover: React.FC<PopoverProps> = ({
+    popover,
+    children,
+    className,
+    style,
+    ...popoverProps
+}) => {
     const [isHovered, setIsHovered] = useState(false)
     const [ setWrapperRef, wrapperRef] = useRefElement<HTMLElement>()
 
     return (
-        <div ref={setWrapperRef}
-            className="popover-wrapper"
+        <div
+            ref={setWrapperRef}
+            style={style}
+            className={cx('popover-wrapper', className)}
             onMouseEnter={() => { setIsHovered(true) }}
             onMouseLeave={() => { setIsHovered(false) }}
         >
