4d1a89d12eb312cd3bf89f3252153ee57c55cb93
Ross Reedstrom <reedstrm@gmail.com>
2022-03-17 16:18:45 -0500

    unbreak preview; fixup specs

--- backend/app/services/qualtrics_launcher.rb
+++ backend/app/services/qualtrics_launcher.rb
@@ -36,14 +36,15 @@ class QualtricsLauncher
     # if we encode them here before generating the md5sum,
     # qualtrics will fail to properly decode them, causing
     # slashes in the "return_to_url" to be percent escaped.
+    # when previewing, there is no launched_study
     raw_query = [
       ['timestamp', Time.now.utc.iso8601],
       ['expiration', 1.hour.from_now.utc.iso8601],
       ['research_id', ResearchId.for_user_id(user_id).id],
       ['return_to_url', frontend_returning_url(study_id: study_id)],
       ['is_testing', !Kinetic.is_production?],
-      ['opted_out', launched_study.opted_out_at],
-      ['consented', launched_study.consent_granted],
+      ['opted_out', launched_study&.opted_out_at],
+      ['consented', launched_study&.consent_granted],
       ['stage_ordinal', stage_ordinal],
       ['study_id', study_id]
     ].map { |k, v| "#{k}=#{v}" }.join('&')
--- backend/spec/services/qualtrics_launcher_spec.rb
+++ backend/spec/services/qualtrics_launcher_spec.rb
@@ -3,21 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe QualtricsLauncher do
+  let(:study1) { create(:study, num_stages: 2) }
+  let(:stage1a) { study1.stages.order(:order)[0] }
+  let(:stage1b) { study1.stages.order(:order)[1] }
   let(:survey_id) { '1Q_B23Y82' }
   let(:secret_key) { 'faY0ccV2dtF19TMS' }
   let(:user_id) { SecureRandom.uuid }
   let(:research_id) { '1234' }
-  let(:study_id) { 4321 }
 
   let(:known_valid_ssotoken) do
-    # Generated using "Generate test token" button on Qualtrics survey
+    # Generated using "Generate test token" button on Qualtrics survey with research_id = 1234 and study_id = 4331
     'ql5aeihGrs3oG5tR0BDuaxSGKRVPmfd5zUK08QLn9Ld5gddl7GVTN%2FtZS3j5%2BTZusMRi0EgMi67ocXYrVJs45lbbHEr2w3tr%2BjDv1qZYztIcfWOxH1Ik3ndIudGOkG%2FcQuF88D%2BlIa%2Br1Q%2BC%2FDFi0tH83whoPh%2B52aRbBYolcNk%3D'
   end
 
+  let(:launch_pad) { LaunchPad.new(study_id: study1.id, user_id: user_id) }
+
   let(:launcher) do
     described_class.new(
-      secret_key: secret_key, survey_id: survey_id, user_id: user_id, study_id: study_id
+      secret_key: secret_key, survey_id: survey_id, user_id: user_id, study_id: study1.id, stage_ordinal: stage1a.order
     )
+  end
+
+  before do
+    # Start study with our user of interest
+    launch_pad.launch_url
   end
 
   it 'shows we can decrypt a Qualtrics-generated token' do
@@ -25,7 +34,7 @@ RSpec.describe QualtricsLauncher do
       decrypt_params(url: "https://blah.com?ssotoken=#{known_valid_ssotoken}", key: secret_key)
     ).to eq(
       'research_id' => research_id,
-      'study_id' => study_id.to_s
+      'study_id' => '4321'
      )
   end
 
@@ -33,9 +42,12 @@ RSpec.describe QualtricsLauncher do
     research_id = ResearchId.for_user_id(user_id).id
     expect(decrypt_params(url: launcher.url, key: secret_key)).to eq(
       'research_id' => research_id,
-      'study_id' => study_id.to_s,
+      'study_id' => study1.id.to_s,
       'is_testing' => true.to_s,
-      'return_to_url' => 'http://example.com/study/land/4321'
+      'return_to_url' => "http://example.com/study/land/#{study1.id}",
+      'opted_out' => '',
+      'consented' => '',
+      'stage_ordinal' => stage1a.order.to_s
     )
   end
 
@@ -47,7 +59,7 @@ RSpec.describe QualtricsLauncher do
     url = URI(url)
     hash = URI.decode_www_form(url.query).to_h
     query_params = URI.decode_www_form(decrypt(token_value: hash['ssotoken'], key: key)).to_h
-    query_params.slice('research_id', 'study_id', 'return_to_url', 'is_testing')
+    query_params.slice('research_id', 'study_id', 'return_to_url', 'is_testing', 'opted_out', 'consented', 'stage_ordinal')
   end
 
   def decrypt(token_value:, key:)
