7a3173c0887ef03ff6a6dde2c6448e5934505994
Ross Reedstrom <reedstrm@gmail.com>
2022-03-17 10:26:01 -0500

    Pass consent and stage number info to qualtrics

--- backend/app/services/qualtrics_launcher.rb
+++ backend/app/services/qualtrics_launcher.rb
@@ -5,11 +5,12 @@ require 'openssl'
 class QualtricsLauncher
   include Rails.application.routes.url_helpers
 
-  def initialize(secret_key:, survey_id:, user_id:, study_id:)
+  def initialize(secret_key:, survey_id:, user_id:, study_id:, stage_ordinal:)
     @secret_key = secret_key
     @survey_id = survey_id
     @user_id = user_id
     @study_id = study_id
+    @stage_ordinal = stage_ordinal
   end
 
   def url
@@ -28,6 +29,7 @@ class QualtricsLauncher
   attr_reader :study_id # study id is the Kinetic model's ID
   attr_reader :survey_id # survey is the Qualtrics ID
   attr_reader :user_id
+  attr_reader :stage_ordinal
 
   def sso_token
     # these values will be url encoded along with the md5hash in the url method.
@@ -40,6 +42,9 @@ class QualtricsLauncher
       ['research_id', ResearchId.for_user_id(user_id).id],
       ['return_to_url', frontend_returning_url(study_id: study_id)],
       ['is_testing', !Kinetic.is_production?],
+      ['opted_out', launched_study.opted_out_at],
+      ['consented', launched_study.consent_granted],
+      ['stage_ordinal', stage_ordinal],
       ['study_id', study_id]
     ].map { |k, v| "#{k}=#{v}" }.join('&')
     hash = md5_hash(raw_query)
@@ -57,6 +62,11 @@ class QualtricsLauncher
   def md5_hash(value)
     hash = OpenSSL::HMAC.digest(OpenSSL::Digest.new('md5'), secret_key, value)
     Base64.strict_encode64(hash)
+  end
+
+  def launched_study
+    # There should be only one not-completed
+    launched_study = LaunchedStudy.where({ user_id: user_id, study_id: study_id, completed_at: nil }).first
   end
 
 end
