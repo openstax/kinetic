88f8a9041581343d13caa7c34db7d9bc69544c80
Ross Reedstrom <reedstrm@gmail.com>
2022-04-28 11:36:48 -0500

    make migration prettyier

--- backend/db/migrate/20220406201351_add_launched_study_ref_to_launched_stage.rb
+++ backend/db/migrate/20220406201351_add_launched_study_ref_to_launched_stage.rb
@@ -3,11 +3,16 @@ class AddLaunchedStudyRefToLaunchedStage < ActiveRecord::Migration[6.1]
     reversible do |dir|
       dir.up do
         add_reference :launched_stages, :launched_study
-        execute 'update launched_stages lsa '\
-                'set launched_study_id = '\
-                '(select lsu.id from stages s join launched_studies lsu '\
-                'on s.study_id = lsu.study_id '\
-                'where s.id = lsa.stage_id and lsu.user_id = lsa.user_id)'
+        execute <<-SQL
+        update launched_stages 
+          set launched_study_id = launched_studies.id
+        from stages, launched_studies
+        where
+          stages.study_id = launched_studies.study_id
+          and stages.id = launched_stages.stage_id
+          and launched_studies.user_id = launched_stages.user_id
+        SQL
+
         change_column_null :launched_stages, :launched_study_id, false
         add_foreign_key :launched_stages, :launched_studies
       end
