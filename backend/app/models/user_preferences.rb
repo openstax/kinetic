a9e10b201628e0dd5aa289d176b7f74ef5a6b0c6
Nathan Stitt <nathan@stitt.org>
2022-08-29 17:03:16 -0500

    add frozen string preamble

--- backend/app/models/user_preferences.rb
+++ backend/app/models/user_preferences.rb
@@ -1,7 +1,7 @@
-class UserPreferences < ApplicationRecord
+# frozen_string_literal: true
 
+class UserPreferences < ApplicationRecord
   def self.for_user_id(user_id)
     find_or_create_by(user_id: user_id)
   end
-
 end
