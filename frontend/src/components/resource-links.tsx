eaf52586083064e914f793356a485a2c119d0c76
Nathan Stitt <nathan@stitt.org>
2022-08-30 14:57:49 -0500

    fix help link

--- frontend/src/components/resource-links.tsx
+++ frontend/src/components/resource-links.tsx
@@ -1,6 +1,4 @@
 import { React } from '@common'
-import envelopeIcon from '@iconify-icons/bi/envelope'
-import { Icon } from './icon'
 
 export const ResourceLinks = () => {
     return (
@@ -16,7 +14,7 @@ export const HelpLink = () => {
     return (
         <>
             <b>Need Help?</b>
-            <a className="text-decoration-none" href="mailto:kinetic@openstax.org?subject=[Kinetic help]"><Icon icon={envelopeIcon} /> Contact us at kinetic@openstax.org</a>
+            <a target="_blank" href="https://openstax.org/contact">Contact us here</a>
         </>
     )
 }
