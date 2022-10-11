07da6d8afe090e7ee07e904bd53991a8415844b6
Nathan Stitt <nathan@stitt.org>
2022-10-11 13:30:38 -0500

    index.html is served outside rails, move to app-root

--- backend/app/controllers/static_controller.rb
+++ backend/app/controllers/static_controller.rb
@@ -4,14 +4,11 @@ class StaticController < ActionController::Base
 
   def catchall
     redirect_to local_dev_path_for_request and return unless Rails.env.production?
+    render file: 'public/app-root.html' and return if has_auth_cookie?
 
-    if has_auth_cookie?
-      render file: 'public/index.html'
-    else
-      # see other is treated as signal that target should be canonical
-      # https://developers.google.com/search/docs/crawling-indexing/301-redirects
-      redirect_to Rails.application.secrets.homepage_url, status: :see_other
-    end
+    # see other is treated as signal that target should be canonical
+    # https://developers.google.com/search/docs/crawling-indexing/301-redirects
+    redirect_to Rails.application.secrets.homepage_url, status: :see_other
   end
 
   def error404
