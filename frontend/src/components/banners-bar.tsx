798b62bdb0765faa3046f037fbf5040432fa30e0
Chris Bendel <chrissbendel@gmail.com>
2022-11-10 12:41:09 -0500

    Fix banner and splash image on mobile

--- frontend/src/components/banners-bar.tsx
+++ frontend/src/components/banners-bar.tsx
@@ -9,9 +9,15 @@ const Banner: React.FC<{
     onRemove: (b: BannerMessage) => void,
     banner: BannerMessage,
 }> = ({ banner, onRemove }) => (
-    <Box className="py-2">
-        <span className="px-1">{banner.message}</span>
-        <Icon css={{ position: 'relative', top: '-15px', right: '-5px' }} icon="x" onClick={() => onRemove(banner)} color="white" />
+    <Box className="py-2 d-flex justify-content-between">
+        <div></div>
+        <span className="align-self-center text-center">{banner.message}</span>
+        <Icon
+            css={{ position: 'relative', top: '-15px', right: '-5px' }}
+            icon="x"
+            onClick={() => onRemove(banner)}
+            color="white"
+        />
     </Box>
 )
 
@@ -26,12 +32,8 @@ export const BannersBar: React.FC = () => {
     return (
         <div
             css={{
-                margin: isMobile ? '0' : '0 50px',
-                display: 'flex',
-                alignItems: 'center',
                 color: 'white',
                 fontFamily: 'Helvetica Neue',
-                textAlign: 'center',
                 backgroundColor: colors.darkTeal,
                 boxShadow: '0px 10px 20px -5px rgba(0, 0, 0, 0.08)',
             }}
