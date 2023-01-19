50c377a8fdf318ac8797fea9d19eb2e6ffbc7183
Chris Bendel <chrissbendel@gmail.com>
2023-01-19 15:46:42 -0500

    Update account details page

--- frontend/src/components/resource-links.tsx
+++ frontend/src/components/resource-links.tsx
@@ -3,7 +3,7 @@ import { React } from '@common'
 export const ResourceLinks = () => {
     return (
         <>
-            <h4>Resource</h4>
+            <h4>Resources</h4>
             <a target="_blank" href="https://help.openstax.org/s/article/Kinetic-FAQs">FAQs</a>
             <a target="_blank" href="https://openstax.org/privacy-policy">Privacy Policy</a>
         </>
