8d21c5a0b3a635b492bca912023ee15b530d739f
Nathan Stitt <nathan@stitt.org>
2023-02-06 15:32:15 -0600

    use UserInfo module and stub accounts response

--- backend/spec/tasks/activity_report_spec.rb
+++ backend/spec/tasks/activity_report_spec.rb
@@ -1,31 +1,51 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
+require 'rails_helper'
 
 Rails.application.load_tasks
 
-RSpec.describe "Rake task for activity:report" do
+RSpec.describe 'Rake task for activity:report' do
 
   let(:study) { create(:study, num_stages: 1) }
   let(:user_id) { SecureRandom.uuid }
   let(:launch_pad) { LaunchPad.new(study_id: study.id, user_id: user_id) }
   let(:task_name) { 'report:activity' }
+  let(:accounts_user) do
+    {
+      id: 1,
+      name: 'test test',
+      uuid: user_id,
+      contact_infos: [
+        {
+          id: 1,
+          type: 'EmailAddress',
+          value: 'participant-test@test.com',
+          is_guessed_preferred: true,
+          is_verified: true
+        }
+      ]
+    }
+  end
+
+  let(:mock_reply) { OpenStruct.new(response: OpenStruct.new(body: { items: [accounts_user] }.to_json)) }
 
   before do
     launch_pad.launch
     launch_pad.land
   end
 
-  it "executes" do
-    stdout = StringIO.new
-    $stdout = stdout
-    Rake::Task[task_name].invoke
-    $stdout = STDOUT
-    Rake.application[task_name].reenable
-    rows = CSV.parse(stdout.string)
+  it 'generates csv with launch' do
+    allow(OpenStax::Accounts::Api).to receive(:search_accounts).and_return(mock_reply)
 
-    expect(rows[0][1]).to eq 'Participant Title'
-    expect(rows[0][2]).to eq 'Researcher Title'
-    expect(rows[1][1]).to eq study.title_for_participants
-    expect(rows[1][2]).to eq study.title_for_researchers
+    task = Rake::Task[task_name]
+    stdout = capture_stdout do
+      task.invoke
+    end
+    task.reenable
+
+    row = CSV.parse(stdout, headers: true).first
+    expect(row['Researcher Title']).to eq study.title_for_researchers
+    expect(row['Participant Title']).to eq study.title_for_participants
+    expect(row['Email']).to eq accounts_user[:contact_infos].first[:value]
   end
 end
