a675650f4305cc9248ec96d24d6374ffda919a50
Nathan Stitt <nathan@stitt.org>
2023-02-07 12:29:59 -0600

    set min height on iframe, not modal

--- frontend/src/screens/studies/modal.tsx
+++ frontend/src/screens/studies/modal.tsx
@@ -28,7 +28,7 @@ const Iframe:React.FC<{ url?: string, onClose: StudyModalProps['onHide'] }> = ({
 
     if (isNil(url)) return null
 
-    return <iframe id="study" css={{ height: '100%', width: '100%', overflow: 'scroll' }} src={url} />
+    return <iframe id="study" css={{ minHeight: 'calc(100vh - 130px)', width: '100%', overflow: 'scroll' }} src={url} />
 }
 
 export const StudyModal:React.FC<StudyModalProps> = ({ onHide, study }) => {
@@ -58,7 +58,7 @@ export const StudyModal:React.FC<StudyModalProps> = ({ onHide, study }) => {
             title={title}
             data-is-study-preview-modal={isPreview}
         >
-            <Modal.Body css={{ padding: 0, minHeight: 'calc(100vh - 130px)' }}>
+            <Modal.Body css={{ padding: 0 }}>
                 {isNil(studyUrl) && <LoadingAnimation />}
                 <Iframe url={studyUrl} onClose={onHide} />
             </Modal.Body>
