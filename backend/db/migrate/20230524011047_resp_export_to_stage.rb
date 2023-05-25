7e1a9750aab19c4576f3d88f1d1bd4127662e5ff
Nathan Stitt <nathan@stitt.org>
2023-05-24 22:05:54 -0500

    delete after table rename

--- backend/db/migrate/20230524011047_resp_export_to_stage.rb
+++ backend/db/migrate/20230524011047_resp_export_to_stage.rb
@@ -1,7 +1,7 @@
 class RespExportToStage < ActiveRecord::Migration[6.1]
   def change
-    AnalysisResponseExport.destroy_all
     rename_table :analysis_response_exports, :response_exports
+    ResponseExport.destroy_all
     remove_column :response_exports, :analysis_id
     add_reference :response_exports, :stage, null: false, foreign_key: true
   end
