37264f3cdb6d9bb3482c38266f25181fcd32048e
Nathan Stitt <nathan@stitt.org>
2023-05-25 09:29:16 -0500

    add a day to the cutoff

--- backend/app/controllers/api/v1/researcher/responses_controller.rb
+++ backend/app/controllers/api/v1/researcher/responses_controller.rb
@@ -5,15 +5,15 @@ class Api::V1::Researcher::ResponsesController < ApplicationController
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   def fetch
-    cutoff = params[:cutoff] ? params[:cutoff].to_date : Date.tomorrow
+    cutoff = params[:cutoff] ? params[:cutoff].to_date : Date.today
     analysis = Analysis.find_by!(api_key: params[:api_key])
 
     # TODO: set is_testing only when coming from outside network
     is_testing = !authenticate_with_http_token do |token, _o|
       Rails.application.secrets.enclave_api_key == token
     end
-
-    responses = analysis.response_exports.for_cutoff(cutoff).where(is_testing: is_testing)
+    # we add a day so that it gets absolutely everything that's contained in the day requested
+    responses = analysis.response_exports.for_cutoff(cutoff + 1.day).where(is_testing: is_testing)
 
     if responses.none? && is_testing
       responses = []
@@ -25,10 +25,11 @@ class Api::V1::Researcher::ResponsesController < ApplicationController
     end
 
     pending = responses.any? { |r| !r.is_complete }
+    urls = responses.filter(&:is_complete).flat_map { |r| r.files.map { |f| url_for(f) } }
 
     api_binding = Api::V1::Bindings::Responses.new(
       status: pending ? 'pending' : 'complete',
-      response_urls: responses.filter(&:is_complete).flat_map { |r| r.files.map { |f| url_for(f) } }
+      response_urls: urls
     )
 
     render json: api_binding, status: :ok
