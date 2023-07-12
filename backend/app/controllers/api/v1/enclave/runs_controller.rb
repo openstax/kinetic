4a4e6192a05cd8ec2cdd41f99d4997a8967bdc5a
Nathan Stitt <nathan@stitt.org>
2023-07-11 21:13:56 -0500

    reformat line

--- backend/app/controllers/api/v1/enclave/runs_controller.rb
+++ backend/app/controllers/api/v1/enclave/runs_controller.rb
@@ -36,22 +36,22 @@ class Api::V1::Enclave::RunsController < Api::V1::BaseController
 
   # stolen from https://github.com/rails/rails/blob/6-1-stable/activestorage/app/controllers/active_storage/direct_uploads_controller.rb
   def upload_results
-    blob = ActiveStorage::Blob.create_before_direct_upload!(**blob_args)
+    args = params.require(:blob).permit(
+      :filename, :byte_size, :checksum, :content_type, metadata: {}
+    ).to_h.symbolize_keys
+    blob = ActiveStorage::Blob.create_before_direct_upload!(**args)
     render json: direct_upload_json(blob)
   end
 
   private
 
-  def blob_args
-    params.require(:blob).permit(:filename, :byte_size, :checksum, :content_type,
-                                 metadata: {}).to_h.symbolize_keys
-  end
-
   def direct_upload_json(blob)
-    blob.as_json(root: false, methods: :signed_id).merge(direct_upload: {
-                                                           url: blob.service_url_for_direct_upload,
-                                                           headers: blob.service_headers_for_direct_upload
-                                                         })
+    blob.as_json(root: false, methods: :signed_id).merge(
+      direct_upload: {
+        url: blob.service_url_for_direct_upload,
+        headers: blob.service_headers_for_direct_upload
+      }
+    )
   end
 
   def find_run
