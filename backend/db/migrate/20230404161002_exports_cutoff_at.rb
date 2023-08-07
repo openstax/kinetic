7482d7e5ce7917c3e1c3978367e3fc7786a6c8a7
Chris Bendel <Chris Bendel>
2023-08-07 12:51:33 -0400

    use change in favor of up/down

--- backend/db/migrate/20230404161002_exports_cutoff_at.rb
+++ backend/db/migrate/20230404161002_exports_cutoff_at.rb
@@ -1,14 +1,7 @@
 class ExportsCutoffAt < ActiveRecord::Migration[6.1]
-  def up
+  def change
     remove_column :studies, :shareable_after_months, :integer
     add_column :studies, :public_on, :datetime, null: true
     add_column :analysis_response_exports, :cutoff_at, :datetime
-
-  end
-
-  def down
-    remove_column :studies, :public_on, :datetime
-    add_column :studies, :shareable_after_months, :integer
-    remove_column :analysis_response_exports, :cutoff_at, :datetime
   end
 end
