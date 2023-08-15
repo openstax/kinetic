fb2e303d7a363a9a7141a6e1e7cf5f4fb13c5ba0
Chris Bendel <Chris Bendel>
2023-08-15 15:53:14 -0400

    Formatting

--- backend/app/controllers/api/v1/open_api_controller.rb
+++ backend/app/controllers/api/v1/open_api_controller.rb
@@ -47,23 +47,23 @@ class Api::V1::OpenApiController < ApplicationController
 
   OPENAPI_CLASSES = [
     Api::V1::OpenApiResponses,
-                      Api::V1::PreferencesOpenApi,
-                      Api::V1::Researcher::StudiesOpenApi,
-                      Api::V1::Researcher::StudyResearchersOpenApi,
-                      Api::V1::Researcher::StagesOpenApi,
-                      Api::V1::Participant::StudiesOpenApi,
-                      Api::V1::EnvironmentOpenApi,
-                      Api::V1::BaseStudiesOpenApi,
-                      Api::V1::Admin::BannersOpenApi,
-                      Api::V1::Admin::ImpersonateOpenApi,
-                      Api::V1::Admin::RewardsOpenApi,
-                      Api::V1::Admin::ResponsesOpenApi,
-                      Api::V1::Admin::StudiesOpenApi,
-                      Api::V1::Researcher::AnalysisOpenApi,
-                      Api::V1::Researcher::ResponsesOpenApi,
-                      Api::V1::Researcher::AnalysisRunOpenApi,
-                      Api::V1::ResearchersOpenApi,
-                      self
+    Api::V1::PreferencesOpenApi,
+    Api::V1::Researcher::StudiesOpenApi,
+    Api::V1::Researcher::StudyResearchersOpenApi,
+    Api::V1::Researcher::StagesOpenApi,
+    Api::V1::Participant::StudiesOpenApi,
+    Api::V1::EnvironmentOpenApi,
+    Api::V1::BaseStudiesOpenApi,
+    Api::V1::Admin::BannersOpenApi,
+    Api::V1::Admin::ImpersonateOpenApi,
+    Api::V1::Admin::RewardsOpenApi,
+    Api::V1::Admin::ResponsesOpenApi,
+    Api::V1::Admin::StudiesOpenApi,
+    Api::V1::Researcher::AnalysisOpenApi,
+    Api::V1::Researcher::ResponsesOpenApi,
+    Api::V1::Researcher::AnalysisRunOpenApi,
+    Api::V1::ResearchersOpenApi,
+    self
   ].freeze
 
   def json
