be7be780f0dbd1004a39c3cfc6a576c1a5971512
Chris Bendel <Chris Bendel>
2023-08-24 12:09:03 -0400

    Add upload avatar endpoint

--- backend/lib/patches/api/v1/bindings/researcher.rb
+++ backend/lib/patches/api/v1/bindings/researcher.rb
@@ -4,7 +4,7 @@ Rails.application.config.to_prepare do
   Api::V1::Bindings::Researcher.class_exec do
     def self.create_from_model(model)
       attributes = model.attributes_for_binding(self)
-      attributes[:avatar_url] = model.avatar_url unless model.avatar.attached?
+      attributes[:avatar_url] = model.avatar_url
       new(attributes)
     end
   end
