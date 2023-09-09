bb98862e567085a2bccd882f0eeac317bc88c474
Chris Bendel <Chris Bendel>
2023-09-09 11:00:42 -0400

    Use intercept study landing

--- frontend/specs/learner.spec.ts
+++ frontend/specs/learner.spec.ts
@@ -30,8 +30,8 @@ test('filtering studies', async ({ browser }) => {
 test('launching study and testing completion', async ({ browser }) => {
     const { adminPage, researcherPage, userPage } = await useUsersContext(browser)
     await interceptStudyLaunch(userPage)
-    // await interceptStudyLand(userPage)
-
+    await interceptStudyLand(userPage)
+    
     await addReward({ page: adminPage, points: 5, prize: 'Pony' })
 
     const studyId = await createStudy({ researcherPage, adminPage })
@@ -90,7 +90,7 @@ test('launching study and completing with no consent', async ({ browser }) => {
     const { userPage, researcherPage, adminPage } = await useUsersContext(browser)
 
     await interceptStudyLaunch(userPage)
-    // await interceptStudyLand(userPage)
+    await interceptStudyLand(userPage)
 
     const studyId = await createStudy({ researcherPage, adminPage })
     await goToPage({ page: userPage, path: `/studies/details/${studyId}` })
