88ce752ab25c6545397d0c24254b5702f2668768
Chris Bendel <Chris Bendel>
2023-11-01 07:46:20 -0400

    Winners modal

--- frontend/src/screens/learner/syllabus-contest.tsx
+++ frontend/src/screens/learner/syllabus-contest.tsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import { BackgroundImage, Badge, Container, Grid, Group, List, Stack, Text, Title } from '@mantine/core';
+import React, { useState } from 'react'
+import {
+    ActionIcon,
+    BackgroundImage,
+    Badge, Button,
+    Container,
+    Grid,
+    Group, Image,
+    List, Modal,
+    Stack,
+    Text,
+    ThemeIcon,
+    Title, Tooltip,
+} from '@mantine/core';
 import { colors } from '@theme';
 import SyllabusContestBackground from './syllabus-contest-background.svg'
 import { StudyCard } from './card';
 import { useLearnerStudies } from './studies';
 import { useCallback, useNavigate } from '@common';
 import { ParticipantStudy } from '@api';
+import dayjs from 'dayjs';
+import { IconInfoCircleFilled } from '@tabler/icons-react';
+import EmailSent from '@images/email-sent.svg'
+import { useLocalStorage } from '@mantine/hooks';
+
+export const useSyllabusContestDates = () => {
+    const isContestActive = dayjs().isBetween(
+        dayjs(),
+        '2023-12-04',
+        'day',
+        '[]'
+    ) || dayjs().isBetween(
+        '2024-02-01',
+        '2024-05-08',
+        'day',
+        '[]'
+    )
+
+    const nextPrizeDate = dayjs().add(1, 'month').set('date', 1)
+
+    console.log(nextPrizeDate.format())
+    return { isContestActive, nextPrizeDate }
+}
 
 export const SyllabusContest: FC<{ studies: ParticipantStudy[] }> = ({ studies }) => {
-    const completedCount = studies.filter(s => !!s.completedAt).length
+    const { isContestActive } = useSyllabusContestDates()
+    // if (!isContestActive) return null
+
     return (
         <BackgroundImage src={SyllabusContestBackground}>
             <Container size='xl' py='5rem' >
@@ -21,28 +58,38 @@ export const SyllabusContest: FC<{ studies: ParticipantStudy[] }> = ({ studies }
                     </Grid.Col>
                 </Grid>
             </Container>
+            <PrizeCycleEnded />
         </BackgroundImage>
     )
 }
 
 export const ContestInfo: FC<{ studies: ParticipantStudy[] }> = ({ studies }) => {
     const completedCount = studies.filter(s => !!s.completedAt).length
+    const { nextPrizeDate } = useSyllabusContestDates()
 
     return (
         <Stack c='white'>
-            <Title order={6}>November Contest</Title>
+            <Title order={6}>{nextPrizeDate.format('MMMM')} Contest</Title>
             <Title order={2}>Join Our Syllabus Contest for a Chance to Win AirPods Pro!</Title>
-            <Group gap='sm'>
-                <Text>You have</Text>
-                <Badge c={colors.text} size='lg' color={colors.gray50}>{completedCount}/{studies.length} Completed</Badge>
-            </Group>
             <Text c={colors.gray70}>Steps:</Text>
             <List c={colors.gray50}>
                 <List.Item>Complete both surveys</List.Item>
                 <List.Item>Automatically entered into prize giveaway</List.Item>
-                <List.Item>Next winner announced on: TODO: dynamic date</List.Item>
+                <List.Item style={{ verticalAlign: 'middle' }}>
+                    Next winner announced on: {nextPrizeDate.format('LL')}
+
+                    <Tooltip withArrow label="Contest ends on December 1, 2023 and restarts on February 1, 2024">
+                        <ThemeIcon radius='lg' size='sm' c={colors.gray70}>
+                            <IconInfoCircleFilled></IconInfoCircleFilled>
+                        </ThemeIcon>
+                    </Tooltip>
+                </List.Item>
                 <List.Item>Come back next month for another chance to win!</List.Item>
             </List>
+            <Group gap='sm'>
+                <Text>You have</Text>
+                <Badge c={colors.text} size='lg' color={colors.gray50}>{completedCount}/{studies.length} Completed</Badge>
+            </Group>
         </Stack>
     )
 }
@@ -54,11 +101,33 @@ export const ContestCards: FC<{ studies: ParticipantStudy[] }> = ({ studies }) =
     if (!studies.length) return null
     return (
         <Group>
-            {studies.map((study) => {
-                return (
-                    <StudyCard key={study.id} onSelect={onStudySelect} study={study} />
-                )
-            })}
+            {studies.map((study) => (
+                <StudyCard key={study.id} onSelect={onStudySelect} study={study} />
+            ))}
         </Group>
+    )
+}
+
+export const PrizeCycleEnded: FC = () => {
+    const [viewed, setViewed] = useLocalStorage({ key: 'viewedSyllabusContestResults', defaultValue: false })
+    const [open, setOpen] = useState(!!viewed)
+
+    const onClose = () => {
+        setOpen(false)
+        setViewed(true)
+    }
+
+    return (
+        <Modal size='75%' centered opened={open} onClose={() => setOpen(false)}>
+            <Stack gap='xl' my='2rem' mx='4rem' align='center'>
+                <Image w='30rem' h='10rem' src={EmailSent} alt='email-sent'/>
+                <Text ta='center'>
+                    This month’s contest prize winner has been selected! We’ll reach out via email - so be sure to keep an eye out. Good luck!
+                </Text>
+                <Button w='15rem' color='purple' c='white' onClick={() => setOpen(false)}>
+                    Return to Dashboard
+                </Button>
+            </Stack>
+        </Modal>
     )
 }
