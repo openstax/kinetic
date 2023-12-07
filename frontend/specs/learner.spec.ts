7551cf055cc87f2a8265f3866d970273ceadd833
Chris Bendel <Chris Bendel>
2023-12-07 09:23:52 -0500

    Remove abort test - revisit in another ticket

--- frontend/specs/learner.spec.ts
+++ frontend/specs/learner.spec.ts
@@ -62,43 +62,44 @@ test('launching study and testing completion', async ({ browser }) => {
     await expect(userPage).not.toHaveSelector('testId=launch-study', { timeout: 200 })
 })
 
-test('launching study and aborting it', async ({ browser }) => {
-    const adminPage = await useAdminPage(browser)
-    const userPage = await useUserPage(browser)
-    const researcherPage = await useResearcherPage(browser)
-
-    await interceptStudyLaunch(userPage)
-
-    const studyId = await createStudy({ researcherPage, adminPage })
-    await goToPage({ page: userPage, path: `/studies/details/${studyId}` })
-
-    await userPage.click('testId=launch-study')
-    await userPage.waitForLoadState('networkidle')
-
-    await goToPage({ page: userPage, path: `/study/land/${studyId}?abort=true` })
-    await userPage.waitForSelector('testId=aborted-msg')
-    await expect(userPage).toMatchText(/Try again later/)
-
-    await userPage.getByRole('button', { name: /Go back to dashboard/i }).click()
-
-    // Our study is under "Learning"
-    await userPage.click('testId=Learning')
-
-    await expect(userPage).toHaveSelector(`[data-study-id="${studyId}"][data-is-completed="false"]`)
-    await userPage.click(`[data-study-id="${studyId}"]`)
-
-    // should have navigated
-    await userPage.waitForURL(`**/studies/details/${studyId}`)
-
-    // now mark complete with consent granted
-    await goToPage({ page: userPage, path: `/study/land/${studyId}?consent=true` })
-
-    await userPage.click('testId=view-studies')
-    await userPage.click('testId=Learning')
-    await expect(userPage).toHaveSelector(`[data-study-id="${studyId}"][data-is-completed="true"]`)
-    await userPage.click(`[data-study-id="${studyId}"]`)
-    await expect(userPage).not.toHaveSelector('testId=launch-study', { timeout: 200 })
-})
+// TODO Revisit aborting in future ticket, currently not being used
+// test('launching study and aborting it', async ({ browser }) => {
+//     const adminPage = await useAdminPage(browser)
+//     const userPage = await useUserPage(browser)
+//     const researcherPage = await useResearcherPage(browser)
+//
+//     await interceptStudyLaunch(userPage)
+//
+//     const studyId = await createStudy({ researcherPage, adminPage })
+//     await goToPage({ page: userPage, path: `/studies/details/${studyId}` })
+//
+//     await userPage.click('testId=launch-study')
+//     await userPage.waitForLoadState('networkidle')
+//
+//     await goToPage({ page: userPage, path: `/study/land/${studyId}?abort=true` })
+//     await userPage.waitForSelector('testId=aborted-msg')
+//     await expect(userPage).toMatchText(/Try again later/)
+//
+//     await userPage.getByRole('button', { name: /Go back to dashboard/i }).click()
+//
+//     // Our study is under "Learning"
+//     await userPage.click('testId=Learning')
+//
+//     await expect(userPage).toHaveSelector(`[data-study-id="${studyId}"][data-is-completed="false"]`)
+//     await userPage.click(`[data-study-id="${studyId}"]`)
+//
+//     // should have navigated
+//     await userPage.waitForURL(`**/studies/details/${studyId}`)
+//
+//     // now mark complete with consent granted
+//     await goToPage({ page: userPage, path: `/study/land/${studyId}?consent=true` })
+//
+//     await userPage.click('testId=view-studies')
+//     await userPage.click('testId=Learning')
+//     await expect(userPage).toHaveSelector(`[data-study-id="${studyId}"][data-is-completed="true"]`)
+//     await userPage.click(`[data-study-id="${studyId}"]`)
+//     await expect(userPage).not.toHaveSelector('testId=launch-study', { timeout: 200 })
+// })
 
 test('launching study and completing with no consent', async ({ browser }) => {
     const adminPage = await useAdminPage(browser)
@@ -125,7 +126,6 @@ test('launching study and completing with no consent', async ({ browser }) => {
     await userPage.waitForURL(`**/studies`)
 
     // Our study is under "Learning"
-    await userPage.click('testId=view-studies')
     await userPage.click('testId=Learning')
 
     await expect(userPage).toHaveSelector(`[data-study-id="${studyId}"][data-is-completed="true"]`)
