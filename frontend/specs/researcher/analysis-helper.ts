321e27803a78d30da0713d0f07d7934010635b62
Chris Bendel <Chris Bendel>
2023-12-18 07:51:26 -0500

    Pass description

--- frontend/specs/researcher/analysis-helper.ts
+++ frontend/specs/researcher/analysis-helper.ts
@@ -13,7 +13,7 @@ export const createAnalysis = async({ researcherPage, adminPage, withStudy = fal
     const description = faker.commerce.productName()
 
     if (withStudy) {
-        const studyId = await createStudy({ researcherPage, adminPage, name })
+        const studyId = await createStudy({ researcherPage, adminPage, name, description })
         await goToPage({ page: researcherPage, path: `/analysis/edit/new?studyId=${studyId}` })
         await expect(researcherPage.locator('[name=title]')).toMatchText(name)
         await expect(researcherPage.locator('[name=description]')).toMatchText(description)
