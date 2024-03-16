ec63e60c105b9415d4a1efd05209ab3b8f791eb6
Chris Bendel <Chris Bendel>
2024-03-16 18:19:16 -0400

    Test

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.require_master_key = false
+
   # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.perform_caching = false
