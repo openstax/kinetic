fe5ead3911b59c235602b0a7a3e9b06493dac51c
Chris Bendel <Chris Bendel>
2024-03-16 11:51:47 -0400

    Test

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -11,7 +11,5 @@ require 'unique_token'
 Dir[Rails.root.join('lib', 'patches', '**', '*.rb')].each { |f| require f }
 Dir[Rails.root.join('lib', 'validators', '**', '*.rb')].each { |f| require f }
 
-puts "WHATSUP!"
-puts Rails.env
 # Initialize the Rails application.
 Rails.application.initialize!
