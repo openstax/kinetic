bb3faab06f50b9a4a0146aa6fa54986872f8b611
Chris Bendel <Chris Bendel>
2024-03-28 13:17:56 -0400

    n+1?

--- backend/app/services/learner_activity_report.rb
+++ backend/app/services/learner_activity_report.rb
@@ -48,7 +48,7 @@ class LearnerActivityReport
   end
 
   def build_rows(csv, users, launches)
-    launches.includes(:stage, study: :first_launched_study).find_each do |launch|
+    launches.includes(:stage, :research_id, study: :first_launched_study).find_each do |launch|
       next if launch.stage.study.first_launched_study.opted_out_at
 
       account = users[launch.user_id] || {}
@@ -73,7 +73,7 @@ class LearnerActivityReport
 
   def generate_report(csv)
     launches = LaunchedStage
-                 .joins(:research_id, stage: :study)
+                 .joins(stage: :study)
                  .where('first_launched_at >= ?', (@months_ago || 1).to_i.months.ago)
 
     users = get_users(launches)
