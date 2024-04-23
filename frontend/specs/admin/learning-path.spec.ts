26c9f8338e865b5544a4ddb31a11c94fac8fc52f
Chris Bendel <Chris Bendel>
2024-04-23 10:15:29 -0400

    Test

--- frontend/specs/admin/learning-path.spec.ts
+++ frontend/specs/admin/learning-path.spec.ts
@@ -1,11 +1,13 @@
-import { createStudy, faker, goToPage, test, useAdminPage, useResearcherPage } from '../test';
+import { faker, goToPage, test, useAdminPage } from '../test';
+
+export const learningPathName = faker.internet.httpMethod() + ' ' + faker.internet.color() + ' ' + faker.internet.userAgent()
 
 test('creates a learning path', async ({ browser }) => {
     const adminPage = await useAdminPage(browser)
 
     await goToPage({ page: adminPage, path: '/admin/manage-learning-paths' })
 
-    await adminPage.getByLabel('Label').fill(faker.word.adjective() + ' ' + faker.word.noun())
+    await adminPage.getByLabel('Label').fill(learningPathName)
     await adminPage.getByLabel('Description').fill(faker.word.adverb() + ' ' + faker.word.interjection())
     await adminPage.getByLabel('Badge ID').fill('SAJSINa7DGDaC4D')
 
@@ -14,16 +16,11 @@ test('creates a learning path', async ({ browser }) => {
 
 test('edits a learning path', async ({ browser }) => {
     const adminPage = await useAdminPage(browser)
-    const researcherPage = await useResearcherPage(browser)
-
-    const name = faker.vehicle.bicycle() + ' ' + faker.vehicle.vin()
-    await createStudy({ researcherPage, adminPage, name })
 
     await goToPage({ page: adminPage, path: '/admin/manage-learning-paths' })
     await adminPage.getByPlaceholder('Select a learning path, or create a new one below').click()
-    await adminPage.getByRole('option').first().click()
+    await adminPage.getByText(learningPathName).click()
 
-    await adminPage.getByLabel('Label').fill(faker.word.adjective() + ' ' + faker.word.noun())
     await adminPage.getByLabel('Description').fill(faker.word.adverb() + ' ' + faker.word.interjection())
     await adminPage.getByText('Update Learning Path').click()
 })
