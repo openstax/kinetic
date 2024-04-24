293f21fd984ac751381fa38f14b4bd05ebf15c15
Chris Bendel <Chris Bendel>
2024-04-24 15:01:25 -0400

    Use where instead of find_by

--- backend/db/migrate/20240415134102_add_order_to_learning_paths.rb
+++ backend/db/migrate/20240415134102_add_order_to_learning_paths.rb
@@ -2,16 +2,16 @@ class AddOrderToLearningPaths < ActiveRecord::Migration[7.1]
   def up
     add_column :learning_paths, :order, :integer
 
-    LearningPath.find_by(label: 'Personal Finance').update(order: 0)
-    LearningPath.find_by(label: 'Growth & Resilience').update(order: 1)
-    LearningPath.find_by(label: 'Mental Agility').update(order: 2)
-    LearningPath.find_by(label: 'Learning Persistence').update(order: 3)
-    LearningPath.find_by(label: 'Productivity').update(order: 4)
-    LearningPath.find_by(label: 'Interpersonal Skills').update(order: 5)
-    LearningPath.find_by(label: 'Study Strategies').update(order: 6)
-    LearningPath.find_by(label: 'STEM Careers').update(order: 7)
-    LearningPath.find_by(label: 'Biology Learning').update(order: 8)
-    LearningPath.find_by(label: 'Future Careers').update(order: 9)
+    LearningPath.where(label: 'Personal Finance').update(order: 0)
+    LearningPath.where(label: 'Growth & Resilience').update(order: 1)
+    LearningPath.where(label: 'Mental Agility').update(order: 2)
+    LearningPath.where(label: 'Learning Persistence').update(order: 3)
+    LearningPath.where(label: 'Productivity').update(order: 4)
+    LearningPath.where(label: 'Interpersonal Skills').update(order: 5)
+    LearningPath.where(label: 'Study Strategies').update(order: 6)
+    LearningPath.where(label: 'STEM Careers').update(order: 7)
+    LearningPath.where(label: 'Biology Learning').update(order: 8)
+    LearningPath.where(label: 'Future Careers').update(order: 9)
   end
 
   def down
