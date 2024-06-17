661dc3991f277ced2c224bf54d9cfb1191309b49
Coder_Srinivas <srinivas.babu364@gmail.com>
2024-06-17 08:50:38 -0500

    Removed all instances of useRef

--- frontend/src/common.ts
+++ frontend/src/common.ts
@@ -9,6 +9,6 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom'
 export * from '@nathanstitt/sundry/base'
 
 export {
-    React, ReactDOM, Box, styled, useState, useEffect, useContext, useMemo, useCallback, useRef, Component,
+    React, ReactDOM, Box, styled, useState, useEffect, useContext, useMemo, useCallback, Component,
     css, keyframes, cx, useLocation, useNavigate, useParams,
 }
