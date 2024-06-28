427c8f081138c6d673d98c7cf0ecb3c70fdb576d
divitcr7 <divit.patidar@gmail.com>
2024-06-28 12:10:47 -0500

    adding the ticket link  in PR description

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,52 +1,156 @@
-import { React } from '@common'
-import { HelpLink, ResourceLinks } from './resource-links'
-import { colors } from '@theme'
-import { useIsMobileDevice } from '@lib';
-import { Anchor, Box, Container, Flex, Group, Image, Stack, Text, Title } from '@mantine/core';
-import { IconBrandFacebookFilled, IconBrandInstagram, IconBrandTwitterFilled } from '@tabler/icons-react';
+import { React } from "@common";
+import { HelpLink, ResourceLinks } from "./resource-links";
+import { colors } from "@theme";
+import { useIsMobileDevice } from "@lib";
+import {
+    Divider,
+    Anchor,
+    Box,
+    Container,
+    Flex,
+    Group,
+    Image,
+    Stack,
+    Text,
+    Title,
+} from "@mantine/core";
+import {
+    IconBrandFacebookFilled,
+    IconBrandInstagram,
+    IconBrandTwitterFilled,
+} from "@tabler/icons-react";
 
 // @ts-ignore
-import NSFLogoURL from '../images/nsf-logo.webp'
+import NSFLogoURL from "../images/nsf-logo.webp";
 // @ts-ignore
-import IESLogoURL from '../images/ies-logo.webp'
+import IESLogoURL from "../images/ies-logo.webp";
 // @ts-ignore
-import RiceLogoURL from '../images/rice-logo.png'
+import RiceLogoURL from "../images/rice-logo.png";
 // @ts-ignore
-import OpenStaxURL from '../images/openstax-logo.png'
+import OpenStaxURL from "../images/openstax-logo.png";
 
 const Funders = () => {
     return (
-        <Container bg='white'>
-            <Stack>
-                <Title order={4} className="fw-bold">Support from Scientific Agencies</Title>
-                <Flex justify='center' align='center' mx='auto'>
-                    <Anchor target="_blank" href="https://ies.ed.gov/">
-                        <Image alt="Institute of Education Sciences logo" src={IESLogoURL}/>
-                    </Anchor>
+        <Container
+            style={{
+                backgroundColor: "white",
+                width: "100%",
+                padding: "16px 32px 40px 32px",
+            }}
+        >
+            <Stack spacing="md">
+                <Title
+                    order={2}
+                    style={{
+                        fontFamily: "Helvetica Neue",
+                        fontSize: "36px",
+                        fontWeight: 700,
+                        lineHeight: "42px",
+                        letterSpacing: "-0.04em",
+                        textAlign: "left",
+                    }}
+                >
+                    Support from scientific agencies
+                </Title>
+
+                <Flex
+                    style={{
+                        width: "100%",
+                        height: "256px",
+                        padding: "16px 0",
+                        gap: "0px",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
                     <Anchor target="_blank" href="https://www.nsf.org/gb/en">
-                        <Image alt="National Science Foundation logo" src={NSFLogoURL} />
+                        <Image
+                            alt="National Science Foundation logo"
+                            src={NSFLogoURL}
+                            width={163.21}
+                            height={120}
+                            style={{ objectFit: "cover", padding: "0px 32px" }}
+                        />
+                    </Anchor>
+
+                    <Divider
+                        orientation="vertical"
+                        style={{
+                            height: "200px",
+                            width: "1px",
+                            borderWidth: "1px",
+                        }}
+                    />
+
+                    <Anchor target="_blank" href="https://ies.ed.gov/">
+                        <Stack
+                            align="center"
+                            spacing="sm"
+                            style={{
+                                width: "462.66px",
+                                height: "186.76px",
+                                padding: "0px 32px",
+                                gap: "16px",
+                            }}
+                        >
+                            <Image
+                                alt="Institute of Education Sciences logo"
+                                src={IESLogoURL}
+                                width={371.5}
+                                height={98.76}
+                                style={{ objectFit: "cover", gap: "0px" }}
+                            />
+                            <Text
+                                size="xs"
+                                style={{
+                                    fontFamily: "Helvetica Neue",
+                                    fontWeight: 400,
+                                    fontSize: "12px",
+                                    lineHeight: "18px",
+                                    textAlign: "center",
+                                    color: "black",
+                                    textDecoration: "none",
+                                }}
+                            >
+                                The research reported here was supported by the
+                                Institute of Education Sciences, U.S. Department
+                                of Education, through Grant R305N210064 to Rice
+                                University. The opinions expressed are those of
+                                the authors and do not represent views of the
+                                Institute or the U.S. Department of Education.
+                            </Text>
+                        </Stack>
+                    </Anchor>
+
+                    <Divider
+                        orientation="vertical"
+                        style={{
+                            height: "200px",
+                            width: "1px",
+                            borderWidth: "1px",
+                        }}
+                    />
+
+                    <Anchor href="https://openstax.org/foundation">
+                        <Text
+                            color="blue"
+                            weight={500}
+                            style={{ textAlign: "center" }}
+                        >
+                            View other Philanthropic supporters
+                        </Text>
                     </Anchor>
                 </Flex>
-
-                <Stack gap='md' mb='md'>
-                    <Anchor href="https://openstax.org/foundation">
-                        View Other Philanthropic Supporters
-                    </Anchor>
-
-                    <Text size='xs' c={colors.text}>
-                        *The research reported here was supported by the Institute of Education Sciences, U.S. Department of Education, through Grant R305N210064 to Rice University. The opinions expressed are those of the authors and do not represent views of the Institute or the U.S. Department of Education.
-                    </Text>
-                </Stack>
             </Stack>
         </Container>
-    )
-}
+    );
+};
 
 export const DesktopFooter: React.FC = () => {
     return (
-        <Box bg='navy' c='white'>
-            <Container bg='navy'>
-                <Group py='xl' align='start' justify='space-between'>
+        <Box bg="navy" c="white">
+            <Container bg="navy">
+                <Group py="xl" align="start" justify="space-between">
                     <HelpLink />
                     <ResourceLinks />
                     <SocialLinks />
@@ -54,64 +158,86 @@ export const DesktopFooter: React.FC = () => {
                 </Group>
             </Container>
         </Box>
-    )
-}
+    );
+};
 
 export const MobileFooter: React.FC = () => {
     return (
-        <Container bg='navy' c='white' style={{
-            'a': {
-                color: colors.white,
-            },
-        }}>
-            <Stack py='lg'>
+        <Container
+            bg="navy"
+            c="white"
+            style={{
+                a: {
+                    color: colors.white,
+                },
+            }}
+        >
+            <Stack py="lg">
                 <HelpLink />
                 <ResourceLinks />
                 <SocialLinks />
                 <Logos />
             </Stack>
         </Container>
-    )
-}
+    );
+};
 
 export const SocialLinks = () => {
     return (
         <Stack>
             <Title order={4}>Follow us</Title>
             <Group>
-                <Anchor c='white' target="_blank" href="https://www.facebook.com/openstax">
+                <Anchor
+                    c="white"
+                    target="_blank"
+                    href="https://www.facebook.com/openstax"
+                >
                     <IconBrandFacebookFilled />
                 </Anchor>
-                <Anchor c='white' target="_blank" href="https://www.instagram.com/openstax/">
+                <Anchor
+                    c="white"
+                    target="_blank"
+                    href="https://www.instagram.com/openstax/"
+                >
                     <IconBrandInstagram />
                 </Anchor>
-                <Anchor c='white' target="_blank" href="https://twitter.com/OpenStax">
+                <Anchor
+                    c="white"
+                    target="_blank"
+                    href="https://twitter.com/OpenStax"
+                >
                     <IconBrandTwitterFilled />
                 </Anchor>
             </Group>
         </Stack>
-    )
-}
+    );
+};
 
 export const Logos = () => {
     return (
-        <Flex direction={{ sm: 'column' }} gap='xl'>
-            <Anchor target='_blank' href='https://www.rice.edu'>
-                <Image alt="Rice University logo" h="30" w='auto' src={RiceLogoURL} />
+        <Flex direction={{ sm: "row" }} gap="xl">
+            <Anchor target="_blank" href="https://www.rice.edu">
+                <Image
+                    alt="Rice University logo"
+                    h="30"
+                    w="auto"
+                    src={RiceLogoURL}
+                />
             </Anchor>
-            <Anchor target='_blank' href='https://www.openstax.org'>
+            <Anchor target="_blank" href="https://www.openstax.org">
                 <Image alt="Open Stax logo" h="30" src={OpenStaxURL} />
             </Anchor>
         </Flex>
-    )
-}
+    );
+};
 
-
-export const Footer: React.FC<{ includeFunders?: boolean }> = ({ includeFunders }) => {
+export const Footer: React.FC<{ includeFunders?: boolean }> = ({
+    includeFunders,
+}) => {
     return (
         <div>
-            {includeFunders && < Funders />}
-            {useIsMobileDevice() ? <MobileFooter/> : <DesktopFooter/>}
+            {includeFunders && <Funders />}
+            {useIsMobileDevice() ? <MobileFooter /> : <DesktopFooter />}
         </div>
-    )
-}
+    );
+};
--- frontend/src/screens/learner/card.tsx
+++ frontend/src/screens/learner/card.tsx
@@ -1,37 +1,37 @@
-import { cx, React, useState } from '@common'
-import { Box, getImageUrl, Icon, MultiSessionBar } from '@components'
-import { useEnvironment, useIsMobileDevice } from '@lib'
-import { getStudyDuration, getStudyPoints, isMultiSession } from '@models'
-import { ParticipantStudy, Study } from '@api'
-import styled from '@emotion/styled'
-import { colors, media } from '@theme'
-import { StudyDetailsPreview } from './details';
-import dayjs from 'dayjs';
-import { Button, Space } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
+import { cx, React, useState } from "@common";
+import { Box, getImageUrl, Icon, MultiSessionBar } from "@components";
+import { useEnvironment, useIsMobileDevice } from "@lib";
+import { getStudyDuration, getStudyPoints } from "@models";
+import { ParticipantStudy, Study } from "@api";
+import styled from "@emotion/styled";
+import { colors, media } from "@theme";
+import { StudyDetailsPreview } from "./details";
+import dayjs from "dayjs";
+import { Button, Space } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 
 interface StudyCardProps {
-    study: ParticipantStudy
+    study: ParticipantStudy;
 }
 
 const Card = styled(Box)({
     minWidth: 400,
     maxWidth: 400,
-    backgroundColor: 'white',
-    padding: '1rem',
-    boxShadow: '0px 6px 10px rgba(0, 0, 0, 0.1)',
-    position: 'relative',
-    color: 'inherit',
-    textDecoration: 'none',
-    cursor: 'pointer',
+    backgroundColor: "white",
+    padding: "1rem",
+    boxShadow: "0px 6px 10px rgba(0, 0, 0, 0.1)",
+    position: "relative",
+    color: "inherit",
+    textDecoration: "none",
+    cursor: "pointer",
     minHeight: 500,
     maxHeight: 500,
     borderRadius: 8,
-    overflow: 'hidden',
-    '&:hover': {
-        boxShadow: '0px 8px 10px rgba(0, 0, 0, 0.4)',
+    overflow: "hidden",
+    "&:hover": {
+        boxShadow: "0px 8px 10px rgba(0, 0, 0, 0.4)",
     },
-    '.study-card-image': {
+    ".study-card-image": {
         height: 200,
         minHeight: 200,
         maxHeight: 200,
@@ -39,56 +39,59 @@ const Card = styled(Box)({
     [media.tablet]: {
         minWidth: 275,
         maxWidth: 275,
-        margin: '0 auto',
-        padding: '1rem',
+        margin: "0 auto",
+        padding: "1rem",
         minHeight: 360,
         maxHeight: 360,
-        '.study-card-image': {
-            minHeight: '35%',
-            maxHeight: '35%',
-            height: '35%',
+        ".study-card-image": {
+            minHeight: "35%",
+            maxHeight: "35%",
+            height: "35%",
         },
     },
     [media.mobile]: {
         minWidth: 275,
         maxWidth: 275,
-        margin: '0 auto',
-        padding: '1rem',
+        margin: "0 auto",
+        padding: "1rem",
         minHeight: 360,
         maxHeight: 360,
-        '.study-card-image': {
-            minHeight: '35%',
-            maxHeight: '35%',
-            height: '35%',
+        ".study-card-image": {
+            minHeight: "35%",
+            maxHeight: "35%",
+            height: "35%",
         },
     },
-})
+});
 
 export const Tag: React.FC<{ tag?: string }> = ({ tag }) => {
-    if (!tag) return null
+    if (!tag) return null;
     return (
-        <span className="badge text-dark" css={{ borderRadius: 8, background: colors.gray30 }}>
+        <span
+            className="badge text-dark"
+            css={{ borderRadius: 8, background: colors.gray30 }}
+        >
             {tag}
         </span>
-    )
-}
+    );
+};
 
 const Researcher: React.FC<StudyCardProps> = ({ study }) => {
-    const pi = study.researchers?.find(r => r.role === 'pi')
-    if (!pi) return null
+    const pi = study.researchers?.find((r) => r.role === "pi");
+    if (!pi) return null;
 
     return (
-        <Box className='x-small' padding={{ bottom: 'small' }}>
+        <Box className="x-small" padding={{ bottom: "small" }}>
             {pi.firstName} {pi.lastName}
         </Box>
-    )
-}
+    );
+};
 
 const MultiSession: React.FC<StudyCardProps> = ({ study }) => {
-    if (!isMultiSession(study)) return <span />
+    if (!studyIsMultipart(study)) return <span />;
 
     return (
-        <Box align='center' gap>
+        <Box align="center" gap>
             <Icon
                 height={15}
                 icon="cardMultiple"
@@ -97,129 +100,144 @@ const MultiSession: React.FC<StudyCardProps> = ({ study }) => {
             />
             <span>Multi-Session</span>
         </Box>
-    )
-}
+    );
+};
 
 const CornerRibbon = styled.div({
-    position: 'absolute',
-    inset: '0 auto auto 0',
+    position: "absolute",
+    inset: "0 auto auto 0",
     background: colors.purple,
-    transformOrigin: '100% 0',
-    transform: 'translate(-29.3%) rotate(-45deg)',
+    transformOrigin: "100% 0",
+    transform: "translate(-29.3%) rotate(-45deg)",
     boxShadow: `0 0 0 999px ${colors.purple}`,
-    clipPath: 'inset(0 -100%)',
+    clipPath: "inset(0 -100%)",
     color: colors.white,
-})
+});
 
-const NewStudyFlag: FC<{study: ParticipantStudy}> = ({ study }) => {
-    if (!study.opensAt) return null
-    const isNew = dayjs(study.opensAt).isAfter(dayjs().subtract(7, 'days'))
-    if (!isNew) return null
+const NewStudyFlag: FC<{ study: ParticipantStudy }> = ({ study }) => {
+    if (!study.opensAt) return null;
+    const isNew = dayjs(study.opensAt).isAfter(dayjs().subtract(30, "days"));
+    if (!isNew) return null;
     return (
         <CornerRibbon>
             <small>New Study</small>
         </CornerRibbon>
-    )
-}
+    );
+};
 
 const CompleteFlag: React.FC<StudyCardProps> = ({ study }) => {
-    if (!study.completedAt) return null
+    if (!study.completedAt) return null;
 
     return (
-        <Box gap
+        <Box
+            gap
             align="center"
             padding="default"
             css={{
                 backgroundColor: colors.green,
-                position: 'absolute',
+                position: "absolute",
                 borderBottomLeftRadius: 20,
                 borderTopLeftRadius: 20,
                 right: 0,
                 top: 16,
             }}
         >
-            <Icon icon="checkCircle" color='white' />
+            <Icon icon="checkCircle" color="white" />
             <span>Complete</span>
         </Box>
-    )
-}
+    );
+};
 
 const MultiSessionFlag: FC<StudyCardProps> = ({ study }) => {
-    if (!isMultiSession(study) || !study.stages?.[0].completedAt || !!study.stages?.[1].completedAt) return null
+    if (
+        !studyIsMultipart(study) ||
+        !study.stages?.[0].isCompleted ||
+        study.stages?.[1].isCompleted
+    )
+        return null;
 
     return (
         <div
             css={{
-                position: 'absolute',
+                position: "absolute",
                 borderBottomLeftRadius: 20,
                 borderTopLeftRadius: 20,
                 right: 0,
                 top: 16,
                 width: 250,
-                backgroundColor: 'white',
+                backgroundColor: "white",
                 zIndex: 3,
                 height: 80,
                 padding: 20,
-                display: 'flex',
+                display: "flex",
                 flex: 1,
-                overflow: 'hidden',
-                boxShadow: '0px 4px 8px rgb(0 0 0 / 18%)',
+                overflow: "hidden",
+                boxShadow: "0px 4px 8px rgb(0 0 0 / 18%)",
             }}
         >
             <MultiSessionBar study={study} />
         </div>
-    )
-}
+    );
+};
 
 const FeedbackMultiSessionContainer: FC<StudyCardProps> = ({ study }) => {
     const isMobile = useIsMobileDevice();
 
-    if (!isMultiSession(study)) {
-        return <Space h='md' />
+    if (!studyIsMultipart(study)) {
+        return <Space h="md" />;
     }
 
     return (
         <Box
-            className={cx({ 'xx-small': isMobile })}
-            justify='between'
+            className={cx({ "xx-small": isMobile })}
+            justify="between"
             wrap
-            margin={{ top: 'default' }}
+            margin={{ top: "default" }}
             css={{ minHeight: 35 }}
         >
             <MultiSession study={study} />
         </Box>
-    )
-}
+    );
+};
 
 const PointsAndDuration: FC<StudyCardProps> = ({ study }) => {
     const isMobile = useIsMobileDevice();
 
     return (
-        <Box className={cx({ 'small': !isMobile, 'xx-small': isMobile }, 'mt-auto', 'pt-1')} justify='between' align='center' wrap>
-            <Box gap='small'>
+        <Box
+            className={cx(
+                { small: !isMobile, "xx-small": isMobile },
+                "mt-auto",
+                "pt-1"
+            )}
+            justify="between"
+            align="center"
+            wrap
+        >
+            <Box gap="small">
                 <Tag tag={study.subject} />
             </Box>
-            <Box gap='small'>
-                {isMultiSession(study) && <span>*Total</span>}
+            <Box gap="small">
+                {studyIsMultipart(study) && <span>*Total</span>}
                 <span>{getStudyDuration(study)} min</span>
                 &middot;
                 <span>{getStudyPoints(study)} pts</span>
             </Box>
         </Box>
-    )
-}
+    );
+};
 
 export const StudyCard: React.FC<StudyCardProps> = ({ study }) => {
-    const nav = useNavigate()
-    const onClick = () => nav(`/studies/details/${study.id}`)
-    const env = useEnvironment()
+    const nav = useNavigate();
+    const onClick = () => nav(`/studies/details/${study.id}`);
+    const env = useEnvironment();
 
     return (
         <Card
             as="a"
-            role={'link'}
+            role={"link"}
             className="col study"
-            direction='column'
+            direction="column"
             onClick={onClick}
             data-study-id={study.id}
             data-is-completed={!!study.completedAt}
@@ -229,17 +247,18 @@ export const StudyCard: React.FC<StudyCardProps> = ({ study }) => {
         >
             <CardContent study={study} />
         </Card>
-    )
-}
+    );
+};
 
-const CardContent: FC<{study: ParticipantStudy}> = ({ study }) => {
+const CardContent: FC<{ study: ParticipantStudy }> = ({ study }) => {
     const isMobile = useIsMobileDevice();
 
     return (
         <>
-            <img src={getImageUrl(study.imageId)}
+            <img
+                src={getImageUrl(study.imageId)}
                 alt={study.imageId}
-                className='study-card-image'
+                className="study-card-image"
                 css={{
                     border: `1px solid ${colors.gray50}`,
                     borderRadius: 8,
@@ -252,25 +271,27 @@ const CardContent: FC<{study: ParticipantStudy}> = ({ study }) => {
             <h6>{study.titleForParticipants}</h6>
             <Researcher study={study} />
             <small
-                className={cx({ 'x-small': isMobile })}
-                css={{ color: colors.text, overflowWrap: 'anywhere' }}
+                className={cx({ "x-small": isMobile })}
+                css={{ color: colors.text, overflowWrap: "anywhere" }}
             >
                 {study.shortDescription}
             </small>
             <PointsAndDuration study={study} />
         </>
-    )
-}
+    );
+};
 
-export const StudyCardPreview: FC<{study: Study}> = ({ study }) => {
-    const [showDetails, setShowDetails] = useState<boolean>(false)
+export const StudyCardPreview: FC<{ study: Study }> = ({ study }) => {
+    const [showDetails, setShowDetails] = useState<boolean>(false);
     return (
-        <Card className="col study" direction='column'>
+        <Card className="col study" direction="column">
             <CardContent study={study as ParticipantStudy} />
             <Button
-                variant='outline'
-                mt='auto'
-                onClick={() => {setShowDetails(true)}}
+                variant="outline"
+                mt="auto"
+                onClick={() => {
+                    setShowDetails(true);
+                }}
             >
                 Preview Study Details
             </Button>
@@ -281,5 +302,5 @@ export const StudyCardPreview: FC<{study: Study}> = ({ study }) => {
                 preview={true}
             />
         </Card>
-    )
-}
+    );
+};
