ae73b8a271ecc6452e23611b7bf8589f4e449130
divitcr7 <divit.patidar@gmail.com>
2024-07-02 17:05:40 -0500

    scroll to top added, cards with completed tag will come at the end, ”New Study” ribbon will display for the studies till 30 days , Changed  heading “All Studies” to “View all studies” in learner.tsx

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,52 +1,121 @@
-import { React } from '@common'
-import { HelpLink, ResourceLinks } from './resource-links'
-import { colors } from '@theme'
+import { React } from '@common';
+import { HelpLink, ResourceLinks } from './resource-links';
 import { useIsMobileDevice } from '@lib';
-import { Anchor, Box, Container, Flex, Group, Image, Stack, Text, Title } from '@mantine/core';
-import { IconBrandFacebookFilled, IconBrandInstagram, IconBrandTwitterFilled } from '@tabler/icons-react';
+import {
+    Divider,
+    Anchor,
+    Box,
+    Container,
+    Flex,
+    Group,
+    Image,
+    Stack,
+    Text,
+    Title,
+} from '@mantine/core';
+import {
+    IconBrandFacebookFilled,
+    IconBrandInstagram,
+    IconBrandTwitterFilled,
+} from '@tabler/icons-react';
 
 // @ts-ignore
-import NSFLogoURL from '../images/nsf-logo.webp'
+import NSFLogoURL from '../images/nsf-logo.webp';
 // @ts-ignore
-import IESLogoURL from '../images/ies-logo.webp'
+import IESLogoURL from '../images/ies-logo.webp';
 // @ts-ignore
-import RiceLogoURL from '../images/rice-logo.png'
+import RiceLogoURL from '../images/rice-logo.png';
 // @ts-ignore
-import OpenStaxURL from '../images/openstax-logo.png'
+import OpenStaxURL from '../images/openstax-logo.png';
 
 const Funders = () => {
     return (
-        <Container bg='white'>
-            <Stack>
-                <Title order={4} className="fw-bold">Support from Scientific Agencies</Title>
-                <Flex justify='center' align='center' mx='auto'>
-                    <Anchor target="_blank" href="https://ies.ed.gov/">
-                        <Image alt="Institute of Education Sciences logo" src={IESLogoURL}/>
-                    </Anchor>
+        <Container bg="white" w="100%" p="md">
+            <Stack gap="md">
+                <Title
+                    order={2}
+                    fz={36}
+                    fw={700}
+                    lh="42px"
+                    ta="left"
+                    ff="Helvetica Neue"
+                    lts="-0.04em"
+                >
+                    Support from scientific agencies
+                </Title>
+
+                <Flex
+                    w="100%"
+                    h={256}
+                    py="md"
+                    justify="space-between"
+                    align="center"
+                >
                     <Anchor target="_blank" href="https://www.nsf.org/gb/en">
-                        <Image alt="National Science Foundation logo" src={NSFLogoURL} />
+                        <Image
+                            alt="National Science Foundation logo"
+                            src={NSFLogoURL}
+                            w={163.21}
+                            h={120}
+                            fit="cover"
+                            px="md"
+                        />
+                    </Anchor>
+
+                    <Divider orientation="vertical" h={200} w={1} />
+
+                    <Anchor target="_blank" href="https://ies.ed.gov/">
+                        <Stack
+                            align="center"
+                            gap="sm"
+                            w={462.66}
+                            h={186.76}
+                            px="md"
+                        >
+                            <Image
+                                alt="Institute of Education Sciences logo"
+                                src={IESLogoURL}
+                                w={371.5}
+                                h={98.76}
+                                fit="cover"
+                            />
+                            <Text
+                                size="xs"
+                                ff="Helvetica Neue"
+                                fw={400}
+                                fz={12}
+                                lh="18px"
+                                ta="center"
+                                c="black"
+                            >
+                                The research reported here was supported by the
+                                Institute of Education Sciences, U.S. Department
+                                of Education, through Grant R305N210064 to Rice
+                                University. The opinions expressed are those of
+                                the authors and do not represent views of the
+                                Institute or the U.S. Department of Education.
+                            </Text>
+                        </Stack>
+                    </Anchor>
+
+                    <Divider orientation="vertical" h={200} w={1} />
+
+                    <Anchor href="https://openstax.org/foundation">
+                        <Text c="blue" fw={500} ta="center">
+                            View other Philanthropic supporters
+                        </Text>
                     </Anchor>
                 </Flex>
-
-                <Stack gap='md' mb='md'>
-                    <Anchor href="https://openstax.org/foundation">
-                        View Other Philanthropic Supporters
-                    </Anchor>
-
-                    <Text size='xs' c={colors.text}>
-                        *The research reported here was supported by the Institute of Education Sciences, U.S. Department of Education, through Grant R305N210064 to Rice University. The opinions expressed are those of the authors and do not represent views of the Institute or the U.S. Department of Education.
-                    </Text>
-                </Stack>
             </Stack>
         </Container>
-    )
-}
+    );
+};
 
 export const DesktopFooter: React.FC = () => {
     return (
-        <Box bg='navy' c='white'>
-            <Container bg='navy'>
-                <Group py='xl' align='start' justify='space-between'>
+        <Box bg="navy" c="white">
+            <Container bg="navy">
+                <Group py="xl" align="start" justify="space-between">
                     <HelpLink />
                     <ResourceLinks />
                     <SocialLinks />
@@ -54,64 +123,78 @@ export const DesktopFooter: React.FC = () => {
                 </Group>
             </Container>
         </Box>
-    )
-}
+    );
+};
 
 export const MobileFooter: React.FC = () => {
     return (
-        <Container bg='navy' c='white' style={{
-            'a': {
-                color: colors.white,
-            },
-        }}>
-            <Stack py='lg'>
+        <Container bg="navy" c="white">
+            <Stack py="lg">
                 <HelpLink />
                 <ResourceLinks />
                 <SocialLinks />
                 <Logos />
             </Stack>
         </Container>
-    )
-}
+    );
+};
 
 export const SocialLinks = () => {
     return (
         <Stack>
             <Title order={4}>Follow us</Title>
             <Group>
-                <Anchor c='white' target="_blank" href="https://www.facebook.com/openstax">
+                <Anchor
+                    c="white"
+                    target="_blank"
+                    href="https://www.facebook.com/openstax"
+                >
                     <IconBrandFacebookFilled />
                 </Anchor>
-                <Anchor c='white' target="_blank" href="https://www.instagram.com/openstax/">
+                <Anchor
+                    c="white"
+                    target="_blank"
+                    href="https://www.instagram.com/openstax/"
+                >
                     <IconBrandInstagram />
                 </Anchor>
-                <Anchor c='white' target="_blank" href="https://twitter.com/OpenStax">
+                <Anchor
+                    c="white"
+                    target="_blank"
+                    href="https://twitter.com/OpenStax"
+                >
                     <IconBrandTwitterFilled />
                 </Anchor>
             </Group>
         </Stack>
-    )
-}
+    );
+};
 
 export const Logos = () => {
     return (
-        <Flex direction={{ sm: 'column' }} gap='xl'>
-            <Anchor target='_blank' href='https://www.rice.edu'>
-                <Image alt="Rice University logo" h="30" w='auto' src={RiceLogoURL} />
+        <Flex direction={{ sm: 'row' }} gap="xl">
+            <Anchor target="_blank" href="https://www.rice.edu">
+                <Image
+                    alt="Rice University logo"
+                    h={30}
+                    w="auto"
+                    src={RiceLogoURL}
+                />
             </Anchor>
-            <Anchor target='_blank' href='https://www.openstax.org'>
-                <Image alt="Open Stax logo" h="30" src={OpenStaxURL} />
+            <Anchor target="_blank" href="https://www.openstax.org">
+                <Image alt="Open Stax logo" h={30} src={OpenStaxURL} />
             </Anchor>
         </Flex>
-    )
-}
+    );
+};
 
-
-export const Footer: React.FC<{ includeFunders?: boolean }> = ({ includeFunders }) => {
+export const Footer: React.FC<{ includeFunders?: boolean }> = ({
+    includeFunders,
+}) => {
     return (
         <div>
-            {includeFunders && < Funders />}
-            {useIsMobileDevice() ? <MobileFooter/> : <DesktopFooter/>}
+            {includeFunders && <Funders />}
+            {useIsMobileDevice() ? <MobileFooter /> : <DesktopFooter />}
         </div>
-    )
-}
+    );
+};
--- frontend/src/screens/learner.tsx
+++ frontend/src/screens/learner.tsx
@@ -1,48 +1,66 @@
-import { React } from '@common'
-import { ParticipantStudy } from '@api'
-import { Footer, TopNavBar } from '@components'
-import { useEnvironment, useIsMobileDevice } from '@lib'
-import { useParticipantStudies, useSearchStudies } from './learner/studies'
-import { StudyCard } from './learner/card'
-import { StudyDetails } from './learner/details'
-import { Route, Routes } from 'react-router-dom'
+import { React } from '@common';
+import { FC, useMemo } from 'react';
+import { ParticipantStudy } from '@api';
+import { Footer, TopNavBar } from '@components';
+import { useEnvironment, useIsMobileDevice } from '@lib';
+import { useParticipantStudies, useSearchStudies } from './learner/studies';
+import { StudyCard } from './learner/card';
+import { StudyDetails } from './learner/details';
+import { Route, Routes } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCards, FreeMode, Navigation, Pagination } from 'swiper/modules';
+import { EffectCards, FreeMode } from 'swiper/modules';
 import { LearnerWelcomeModal } from './learner/learner-welcome-modal';
 import { UnsupportedCountryModal } from './learner/unsupported-country-modal';
-import { Badge, Box, Container, Flex, Group, Stack, Text, TextInput, Title } from '@mantine/core';
+import {
+    Badge,
+    Box,
+    Container,
+    Divider,
+    Flex,
+    Group,
+    Stack,
+    Text,
+    TextInput,
+    Title,
+} from '@mantine/core';
 import { IconSearch, IconX } from '@tabler/icons-react';
-import { groupBy } from 'lodash';
-import { colors } from '@theme'
-import { useMemo } from 'react';
-import { orderBy, sortBy, uniqBy } from 'lodash-es';
+import { groupBy, orderBy, uniqBy } from 'lodash-es';
+import ScrollToTopButton from './researcher/ScrollToTopButton';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import { colors } from '@theme';
 
 const HighlightedStudies: FC = () => {
-    const { highlightedStudies } = useParticipantStudies()
-    const isMobile = useIsMobileDevice()
+    const { highlightedStudies } = useParticipantStudies();
+    const isMobile = useIsMobileDevice();
 
-    if (!highlightedStudies.length) return null
+    if (!highlightedStudies.length) return null;
 
     return (
-        <Box bg={colors.navy} py='md'>
+        <Box bg={colors.navy} py="md">
             <Container>
                 <Stack>
-                    <Title c='white' order={2}>Highlighted Studies</Title>
-                    {isMobile ?
-                        <MobileStudyCards studies={highlightedStudies} /> :
+                    <Title c="white" order={2}>
+                        Highlighted Studies
+                    </Title>
+                    {isMobile ? (
+                        <MobileStudyCards studies={highlightedStudies} />
+                    ) : (
                         <DesktopStudyCards studies={highlightedStudies} />
-                    }
+                    )}
                 </Stack>
             </Container>
         </Box>
-    )
-}
+    );
+};
 
 const LearnerDashboard = () => {
-    const env = useEnvironment()
+    const env = useEnvironment();
 
     if (!env.isEligible) {
-        return <UnsupportedCountryModal />
+        return <UnsupportedCountryModal />;
     }
 
     return (
@@ -64,184 +82,216 @@ const LearnerDashboard = () => {
             <HighlightedStudies />
 
             <StudiesContainer />
+            <ScrollToTopButton />
 
             <Footer includeFunders />
         </div>
-    )
-}
+    );
+};
 
-export const SearchBar: FC<{search: string, setSearch: (search: string) => void}> = ({ search, setSearch }) => {
-    const isMobile = useIsMobileDevice()
+export const SearchBar: FC<{
+    search: string;
+    setSearch: (search: string) => void;
+}> = ({ search, setSearch }) => {
+    const isMobile = useIsMobileDevice();
 
     return (
         <TextInput
-            w={isMobile ? '100%' : '400px'}
-            size='lg'
+            width={isMobile ? '100%' : '400px'}
+            size="lg"
             value={search}
             onChange={(event) => setSearch(event.currentTarget.value)}
-            rightSection={search.length ?
-                <IconX onClick={() => setSearch('')} style={{ cursor: 'pointer' }}/> :
-                <IconSearch />
+            rightSection={
+                search.length ? (
+                    <IconX onClick={() => setSearch('')} />
+                ) : (
+                    <IconSearch />
+                )
             }
             placeholder="Search by study title, researcher, or topic name"
         />
-    )
-}
+    );
+};
 
-export const StudiesTitle: FC<{search: string, filteredStudies: ParticipantStudy[]}> = () => {
-    return (
-        <Title order={2}>All Studies</Title>
-    )
-}
+export const StudiesTitle: FC = () => {
+    return <Title order={2}>View all Studies</Title>;
+};
 
-export const SearchResults: FC<{search: string, filteredStudies: ParticipantStudy[]}> = ({ search, filteredStudies }) => {
+export const SearchResults: FC<{
+    search: string;
+    filteredStudies: ParticipantStudy[];
+}> = ({ search, filteredStudies }) => {
     if (!search) {
-        return null
+        return null;
     }
 
-    if (filteredStudies.length == 0) {
-        return (
-            <Title order={4}>
-                Sorry, no results found for '{search}'
-            </Title>
-        )
+    if (filteredStudies.length === 0) {
+        return <Title order={4}>Sorry, no results found for '{search}'</Title>;
     }
 
     return (
         <Title order={4}>
-            {filteredStudies.length} result{filteredStudies.length == 1 ? '' : 's'} for '{search}'
+            {filteredStudies.length} result
+            {filteredStudies.length === 1 ? '' : 's'} for '{search}'
         </Title>
-    )
-}
+    );
+};
 
 export const StudiesContainer = () => {
-    const { search, setSearch, filteredStudies } = useSearchStudies()
+    const { search, setSearch, filteredStudies } = useSearchStudies();
 
     return (
-        <Container my='lg'>
-            <Stack gap='lg'>
-                <Flex justify='space-between' wrap='wrap'>
-                    <StudiesTitle search={search} filteredStudies={filteredStudies} />
+        <Container my="lg">
+            <Stack gap="lg">
+                <Flex justify="space-between" wrap="wrap">
+                    <StudiesTitle />
 
                     <SearchBar search={search} setSearch={setSearch} />
                 </Flex>
 
-                <SearchResults search={search} filteredStudies={filteredStudies} />
+                {/* Divider */}
+                <Divider
+                    my="md"
+                    size="sm"
+                    color="#dbdbdb"
+                    w={864}
+                    style={{ border: '1px solid #dbdbdb' }}
+                />
+
+                <SearchResults
+                    search={search}
+                    filteredStudies={filteredStudies}
+                />
 
                 <StudiesByLearningPath filteredStudies={filteredStudies} />
             </Stack>
         </Container>
-    )
-}
+    );
+};
 
-export const MobileStudyCards: FC<{studies: ParticipantStudy[]}> = ({ studies }) => {
+export const MobileStudyCards: FC<{ studies: ParticipantStudy[] }> = ({
+    studies,
+}) => {
     return (
-        <Box>
+        <Box className="swiper-container">
             <Swiper
-                effect={'cards'}
-                slidesPerView={'auto'}
+                effect="cards"
+                slidesPerView="auto"
                 cardsEffect={{
                     slideShadows: false,
                     perSlideOffset: 14,
                 }}
-                centeredSlides={true}
-                pagination={{
-                    enabled: true,
-                    dynamicBullets: true,
-                    dynamicMainBullets: 5,
-                }}
-                modules={[EffectCards, Pagination]}
-                style={{
-                    paddingBottom: '2rem',
-                    marginBottom: '1rem',
-                }}
+                centeredSlides
+                modules={[EffectCards]}
+                style={{ paddingBottom: '2rem', marginBottom: '1rem' }}
+                pagination={false}
             >
-                {studies.map((study) => (
-                    <SwiperSlide key={study.id} className="pb-1">
-                        <StudyCard study={study} />
-                    </SwiperSlide>
-                ))}
+                {studies
+                    .sort((a, b) =>
+                        a.completedAt === b.completedAt
+                            ? 0
+                            : a.completedAt
+                                ? 1
+                                : -1
+                    )
+                    .map((study) => (
+                        <SwiperSlide key={study.id} className="pb-1">
+                            <StudyCard study={study} />
+                        </SwiperSlide>
+                    ))}
             </Swiper>
         </Box>
-    )
-}
+    );
+};
 
-export const DesktopStudyCards: FC<{studies: ParticipantStudy[]}> = ({ studies }) => {
+export const DesktopStudyCards: FC<{ studies: ParticipantStudy[] }> = ({
+    studies,
+}) => {
     return (
-        <Box>
+        <Box className="swiper-container">
             <Swiper
                 slidesPerView={3}
-                simulateTouch={true}
-                freeMode={true}
-                pagination={{
-                    enabled: true,
-                    dynamicBullets: true,
-                    dynamicMainBullets: 5,
-                    clickable: true,
-                }}
+                simulateTouch
+                freeMode
                 style={{
                     marginBottom: '1rem',
                     paddingBottom: '2rem',
                     paddingLeft: '2rem',
                     paddingRight: '2rem',
                 }}
-                navigation={{
-                    enabled: true,
-                }}
-                modules={[FreeMode, Pagination, Navigation]}
+                modules={[FreeMode]}
             >
-                {studies.map(study => (
-                    <SwiperSlide style={{ padding: '1rem' }} key={study.id}>
-                        <StudyCard study={study} />
-                    </SwiperSlide>
-                ))}
+                {studies
+                    .sort((a, b) =>
+                        a.completedAt === b.completedAt
+                            ? 0
+                            : a.completedAt
+                                ? 1
+                                : -1
+                    )
+                    .map((study) => (
+                        <SwiperSlide style={{ padding: '1rem' }} key={study.id}>
+                            <StudyCard study={study} />
+                        </SwiperSlide>
+                    ))}
             </Swiper>
         </Box>
-    )
-}
+    );
+};
 
-export const StudiesByLearningPath: FC<{filteredStudies: ParticipantStudy[]}> = ({ filteredStudies }) => {
+export const StudiesByLearningPath: FC<{
+    filteredStudies: ParticipantStudy[];
+}> = ({ filteredStudies }) => {
     const [learningPaths, studiesByLearningPath] = useMemo(() => {
         return [
             orderBy(
-                (uniqBy(filteredStudies.map(fs => fs.learningPath), (lp) => lp?.label)),
-                ['completed', 'order'],
-                ['asc', 'asc']
+                uniqBy(
+                    filteredStudies.map((fs) => fs.learningPath),
+                    (lp) => lp?.label
+                ),
+                ['completed'],
+                ['asc']
             ),
             groupBy(filteredStudies, (study) => study.learningPath?.label),
-        ]
-    }, [filteredStudies])
+        ];
+    }, [filteredStudies]);
 
-    const isMobile = useIsMobileDevice()
+    const isMobile = useIsMobileDevice();
 
     return (
-        <Stack gap='lg' data-testid='studies-listing'>
-            {learningPaths.map(learningPath => {
-                if (!learningPath) return null
-                const studies = sortBy(studiesByLearningPath[learningPath.label], (study) => !!study.completedAt)
-
+        <Stack gap="lg" data-testid="studies-listing">
+            {learningPaths.map((learningPath) => {
+                if (!learningPath) return null;
+                const studies = studiesByLearningPath[learningPath.label].sort(
+                    (a, b) =>
+                        a.completedAt === b.completedAt
+                            ? 0
+                            : a.completedAt
+                                ? 1
+                                : -1
+                );
                 return (
                     <Stack key={learningPath.label}>
-                        <Group gap='sm'>
-                            <Title order={3}>
-                                {learningPath.label}
-                            </Title>
+                        <Group gap="sm">
+                            <Title order={3}>{learningPath.label}</Title>
                             <Text span>|</Text>
-                            <Title order={3} fw='300'>
+                            <Title order={3} fw={300}>
                                 {learningPath.description}
                             </Title>
-                            {learningPath.completed ? <Badge c={colors.text} color={colors.green}>Completed</Badge> : null}
+                            {learningPath.completed ? (
+                                <Badge color={colors.green}>Completed</Badge>
+                            ) : null}
                         </Group>
-                        {isMobile ?
-                            <MobileStudyCards studies={studies} /> :
+                        {isMobile ? (
+                            <MobileStudyCards studies={studies} />
+                        ) : (
                             <DesktopStudyCards studies={studies} />
-                        }
+                        )}
                     </Stack>
-                )
+                );
             })}
         </Stack>
-    )
-}
+    );
+};
 
-
-export default LearnerDashboard
+export default LearnerDashboard;
