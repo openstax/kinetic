7652faacfaf06fcb79b48f94b7f929a7c8bafcb4
Coder_Srinivas <srinivas.babu364@gmail.com>
2024-07-18 15:12:43 -0500

    Used id's instead of title to update failing illustrations

--- backend/db/structure.sql
+++ backend/db/structure.sql
@@ -1477,7 +1477,10 @@ ALTER TABLE ONLY public.response_exports
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20240718193802'),
 ('20240718103317'),
+('20240718094704'),
+('20240718093303'),
 ('20240712192733'),
 ('20240712192124'),
 ('20240712191554'),
